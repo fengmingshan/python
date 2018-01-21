50a64823d5001082c620021677b58f8019891add
fengmingshan <fms_python@163.com>
2018-01-21 12:56:25 +0800

    修改Tower_check_v0.1.py

--- Tower_check_v0.1.py
+++ Tower_check_v0.1.py
@@ -8,11 +8,19 @@ import pandas as pd   #导入pandas库
 from pandas import DataFrame   #从pandas库导入数据框这种数据结构
 import numpy as np   #导入numpy库    
 
-    
+old=r'd:\2018年工作\2018年铁塔租费核对\tower_201709.xls'
+new=r'd:\2018年工作\2018年铁塔租费核对\tower_201710.xls'
+house=r'd:\2018年工作\2018年铁塔租费核对\new_house_bill.xls'
+new_tower_bill=\
+{'普通地面塔_H<30':15.8902,'普通地面塔_30≤H<35':18.3433,'普通地面塔_35≤H<40':21.6758,'普通地面塔_40≤H<45':25.4928,'普通地面塔_45≤H<50':29.9715,
+'景观塔_H<20':8.7872,'景观塔_H<20':11.3222,'景观塔_H<20':13.406,'景观塔_H<20':18.2525,'景观塔_H<20':20.9292,
+'简易塔_H≤20':3.9264,'普通楼面塔':4.0753,'楼面抱杆':1.2107}
+new_house_bill=pd.read_excel(house,dtype =str,encoding='utf-8')     #导机房价格
+
+
 def main():  
-    # 导入上月账单和本月账单
-    df_old = pd.read_excel(r'd:\2018年工作\2018年铁塔租费核对\tower_201709.xls',dtype =str,encoding='utf-8')    #导入上月铁塔账单
-    df_new = pd.read_excel(r'd:\2018年工作\2018年铁塔租费核对\tower_201710.xls',dtype =str,encoding='utf-8')    #导入本月铁塔账单    
+    df_old = pd.read_excel(old,dtype =str,encoding='utf-8')     #导入上月账单
+    df_new = pd.read_excel(new,dtype =str,encoding='utf-8')     ##导入本月账单
 
     df_merge=pd.merge(df_old,df_new,how='right',on='产品业务确认单编号',suffixes=('上月','本月')) 
     # 对上月账单和本月账单进行右合并成df_merge，关键字是'产品业务确认单编号'，列标后面添加标识‘左表’和‘右表’
@@ -39,7 +47,6 @@ def main():
     """
     # 创建一个空表df_change，用于存放比对出来发生了变化的数据
     df_change=pd.DataFrame(columns=['是否发生变化','变化内容','铁塔站址名称','铁塔站址编码','上月值','本月值'])
-
     for i in range(10,147,1):        # 定义循环，i=10—146,从第10列开始比较。因为前9列不会发生变化
         df_change_tmp=df_merge.iloc[:,[6+146,7+146,i,i+146]]     # 取左表的第10列和右表的第十列构建一个新DataFrame：df_change_tmp
         df_change_tmp.columns = ['铁塔站址名称','铁塔站址编码','上月值','本月值']   # 修改新表的列标
@@ -49,6 +56,24 @@ def main():
         df_change_tmp=df_change_tmp[df_change_tmp['是否发生变化']==True]    #对df_change_tmp筛选，'属性变化'列=True的内容
         df_change=pd.concat([df_change,df_change_tmp])     #筛选过的df_change_tmp合并在df_change后
     
+    """
+    逐行检查df_new的铁塔价格
+    """
+    # 创建一个空表df_price，用于存放比价格比对的数据
+    df_price=pd.DataFrame(columns=['账期月份','铁塔站址名称','铁塔站址编码','产品类型','机房类型','产品单元数',
+    '计算铁塔价格','对应铁塔基准价格1','计算机房价格','对机房基准价格1','计算配套价格','对配套基准价格1','计算维护费','对应维护费1',])
+    df_price['账期月份']=df_new['账期月份']
+    df_price['铁塔站址名称']=df_new['铁塔站址名称']
+    df_price['铁塔站址编码']=df_new['铁塔站址编码']
+    df_price['产品类型']=df_new['产品类型']
+    df_price['机房类型']=df_new['机房类型']
+    df_price['产品单元数']=df_new['产品单元数']
+    for j in range(0,df_new[:,0].conut()+1,1):
+        df_price.loc[j,'计算铁塔价格']=
+    
+    """
+    将核查结果写入到excel中
+    """
     writer = pd.ExcelWriter(r'd:\2018年工作\2018年铁塔租费核对\核查结果\核查结果.xls') #输出到excel
     df_change.to_excel(writer, '本月变化订单')
     df_delete.iloc[:,0:145].to_excel(writer, '本月退租订单')
