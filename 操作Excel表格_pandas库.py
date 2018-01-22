1af5fdc961f07fa7282f8f1559cdc6d32b22d3bc
fengmingshan <fms_python@163.com>
2018-01-22 20:07:47 +0800

    添加

--- 操作Excel表格_pandas库.py
+++ 操作Excel表格_pandas库.py
@@ -6,11 +6,13 @@ F:/test 目录下有两张表格A,B,分别是几个基站的3G流量和4G流量
 '''
 import pandas as pd      #导入pandas库
 
-df1 = pd.read_excel(r'f:\test\A.xls',encoding='utf-8')    #导入一张excel97,2003表格赋值给df1
-df2= pd.read_excel(r'f:\test\B.xls',encoding='utf-8')    #导入一张excel97,2003表格赋值给df2
+A=r'f:\test\A.xls'
+B=r'f:\test\B.xls'
+df1 = pd.read_excel(A,encoding='utf-8')    #导入一张excel97,2003表格赋值给df1
+df2 = pd.read_excel(B,encoding='utf-8')    #导入一张excel97,2003表格赋值给df2
 #用来打开excel的数据结构pandas.DataFrame，所以在命名变量的时候建议使用df，增加代码可读性
 
-df3=pd.merge(df1,df2,how='right',on='基站名称')
+df3=pd.merge(df1,df2,how='left',on='基站名称')
 #定义一个新的DataFrame df3 来对df1 和df2 进行合并，合并的关键字是基站名称
 #这里的3个参数how是合并方法（right/left/inner不懂的同学复习下数据库的ppt ）on是合并的关键字。
 
@@ -24,7 +26,7 @@ df4['4g流量']=df3['4G流量(Gb)']
 df4['4g用户数']=df3['4G在线用户数']
 
 #需要的表格已经完成，可以输出到表格了
-df4 .to_excel(r'f:\test\1.xls')    #将df4导出一张xls表格
+df4.to_excel(r'f:\test\1.xls')    #将df4导出一张xls表格
 
 #本课思考题：如果我需要一张报表，格式如下’’基站名称‘，’总数据流量‘，’4g渗透比‘。要求输出到2.xls
 #总数据流量=3g流量+4g流量，4g渗透比='4G在线用户数'/'3G在线用户数'
@@ -63,24 +65,37 @@ df2= pd.read_excel(r'f:\test\B.xls',encoding='utf-8')    #导入一张excel97,20
 #注意loc[]方括号里面有两个字段，第一个是行号，第二个是列名。
 #选择行时第二个字段可以不写，但是选择列时第一个字段必须写成loc[:,'xx']表示选所有行
 df1.loc[0:2]    #选取表1的1-3行
-df1.loc[:,'基站名称']   #选取表1的基站名称列
 df2.loc[:,'4G流量(Gb)']   #选取表1的基站名称列
 #这种方法缺点是写代码要写中文的列名不方便
 #当表格比较大的时候，需要一列一列效率很低
 
 #第2种方法使用.iloc
 #iloc格式同loc，但是可以写列号，不用写列名
-a=df1.iloc[:,0]   #选取表1的第一列，注意变量浏览器里的a，实际是两列，第一列是序号
-b=df1.iloc[0]   #选取表1的第一行，同样注意看变量浏览器b也有两行，第一行是行名称。
+df3=df1.iloc[:,0]   #选取表1的第一列，注意变量浏览器里的a，实际是两列，第一列是序号
+df4=df1.iloc[0]   #选取表1的第一行，同样注意看变量浏览器b也有两行，第一行是行名称。
 
-'''
-excel表格相关操作#2  
-表格切片操作:
-'''
-import pandas as pd      #导入pandas库
-from pandas import DataFrame  #从pandas库导入DataFrame数据框
-df3=pd.read_excel(r'f:\test\铁塔产品价格表.xls',encoding='utf-8')    #导入一张excel97,2003表格赋值给df3
-df3.at[3,'铁塔+租赁机房+配套']
-df3.at[4,'挂高']
+
+#第3种方法使用.at（十字交叉切片，最后会返回十字交叉点的单元格的值）
+#下面使用铁塔维护费的表格来演示纵横切片
+df5=pd.read_excel(r'f:\test\weihu.xls',encoding='utf-8')    #导入铁塔维护费表格赋值给df1
+a=df5.at[0,'自建砖混机房']    #对第1行，'自建砖混机房'列进行切片，注意行号是从0开始编，第一行就是0行
+b=df5.at[4,'自建彩钢板机房']    #对第1行，'自建砖混机房'列进行切片，注意行号是从0开始编，第一行就是0行
+
+ #如果要根据行的关键字进行切片，需要重新设置行索引，默认行索引都是0,1,2，3的数字
+df5=df5.set_index('运营商区县')   #将'运营商区县'一列设置为行索引
+c=df5.at['马龙县','一体化机柜']    #下面就可以用关键字进行切片了，
+d=df5.at['宣威市','一体化机房']    #下面就可以用关键字进行切片了，
+
+#第4种方法使用.iat（纵横切片，同第三种，但是切片的关键字只能使用数字序号）
+#下面使用铁塔维护费的表格来演示纵横切片
+df6=pd.read_excel(r'f:\test\weihu.xls',encoding='utf-8')    #导入一张excel97,2003表格赋值给df1
+
+e=df6.iat[0,0]  #第1行第1列十字切片，得到第一个县名’富源县‘
+f=df6.iat[1,1]  #第2行第2列十字切片，得到的是麒麟区,自建砖混机房的价格。
+g=df6.iat[3,2]  #第4行第3列十字切片，得到的是会泽县,自建框架机房的价格。
+
+
+
+
 
 
