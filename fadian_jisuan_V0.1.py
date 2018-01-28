691b8daed7adad4232fd2d766fab961f427514cc
fengmingshan <fms_python@163.com>
2018-01-28 22:16:05 +0800

    完善：fadian_jisuan_V0.1.py
    Signed-off-by: fengmingshan <fms_python@163.com>

--- fadian_jisuan_V0.1.py
+++ fadian_jisuan_V0.1.py
@@ -8,6 +8,7 @@ import pandas as pd   #导入pandas库
 from datetime import datetime #导入时间格式datetime
 from datetime import date #导入时间格式date
 from datetime import timedelta #导入时间格式date
+import datetime
 
 
 fadian=r'd:\2018年工作\2018年铁塔发电费用核对\fadian_2017-11.xls'
@@ -32,6 +33,10 @@ def main():
     col_name.insert(col_name.index('发电日期'),'对应电信网管站址')
     col_name.insert(col_name.index('备注'),'发电是否及时')
     df_fadian=df_fadian.reindex(columns=col_name) 
+    df_tmp = pd.read_excel(fadian,dtype=str,usecols=[6,7])
+    df_fadian['站址编码']=df_tmp['站址编码']
+    df_fadian['资源系统编码']=df_tmp['资源系统编码']
+
     df_fadian['结算时长']=0
     df_fadian['结算次数']=0
     df_fadian['共享系数']=0
@@ -43,19 +48,20 @@ def main():
     df_fadian.drop(labels=['共享系数'], axis=1,inplace = True)  #原表中删除'共享系数'一列
     df_fadian.insert(45,'共享系数',tmp)     #将‘共享系数’列插回到原表第46列
     df_fadian.fillna(0,inplace = True)
-
-
+    d1=datetime.datetime.strptime('1970-01-01 00:00:00','%Y-%m-%d %H:%M:%S')    #生成停电时间为0时的时间d1
+    cishu=df_fadian['资源系统编码'].value_counts()
     for i in range(0,len(df_fadian),1):
-        if  df_fadian.loc[i,'市电停电时间']=1970-01-01 00:00:00
-            df_fadian.loc[i,'剔除蓄电池保障3小时的时长']=df_fadian.loc[i,'铁塔审核发电结束时间']-df_fadian.loc[i,'铁塔审核发电开始时间']
-        elif :
-            df_fadian.loc['剔除蓄电池保障3小时的时长']=df_fadian.loc['铁塔审核发电结束时间']-df_fadian.loc['市电停电时间']-pd.Timedelta('0 days 3:0:00')
-    
-    sheet=df_fadian.iloc[1,13][:-12]    #从发电表取第14列第1行发电月份的2017-11，做为输出excel的sheet名称
+        df_fadian.loc[i,'当月发电次数']=cishu[df_fadian.loc[i,'资源系统编码']]
+        if  df_fadian.loc[i,'市电停电时间']==d1:  #当停电时间=0，即没有获取到停电时间
+            fadian_time=df_fadian.loc[i,'铁塔审核发电结束时间']-df_fadian.loc[i,'铁塔审核发电开始时间']
+            df_fadian.loc[i,'剔除蓄电池保障3小时的时长']=round(fadian_time.total_seconds()/60,0) 
+        else:
+            fadian_time=df_fadian.loc[i,'铁塔审核发电结束时间']-df_fadian.loc[i,'市电停电时间']-pd.Timedelta('0 days 3:0:00')
+            df_fadian.loc[i,'剔除蓄电池保障3小时的时长']==round(fadian_time.total_seconds()/60)
+        
+    d2=df_fadian.iloc[1,12]
+    sheet= d2.strftime('%Y-%m-%d %H:%M:%S')[:-12]   #从发电表取第14列第1行发电时间，格式化后，切片出月份：2017-11，做为输出excel的sheet名称
     write_xls(df_fadian,sheet)
-
-
-
 
 if __name__=='__main__':        #python最终封装，python的固定格式
     main()
