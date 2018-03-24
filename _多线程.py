755acdbdba080a2d013c2316a20c5c5f6beae835
fengmingshan <fms_python@163.com>
2018-03-24 19:45:06 +0800

    修改1处错误：采集爱立信基站断站_修改版

---
+++ _多线程.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 18:02:42 2018
+
+@author: Administrator
+"""
+
+import time
+import threading
+
+# 新线程执行的代码:
+def loop():     #定义传入子线程的函数
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
+# =============================================================================
+# 由于任何进程默认就会启动一个线程，我们把该线程称为主线程，主线程又可以启动新的线程，
+# Python的threading模块有个current_thread()函数，它永远返回当前线程的实例。
+# 主线程实例的名字叫MainThread，子线程的名字在创建时指定，我们用LoopThread命名子线程。
+# 名字仅仅在打印时用来显示，完全没有其他意义，如果不起名字Python就自动给线程命名为Thread-1，Thread-2……
+# =============================================================================
+
+# =============================================================================
+# 多线程和多进程最大的不同在于，多进程中，同一个变量，各自有一份拷贝存在于每个进程中，互不影响。
+# 而多线程中，所有变量都由所有线程共享，所以，任何一个变量都可以被任何一个线程修改，
+# 因此，线程之间共享数据最大的危险在于多个线程同时改一个变量，把内容给改乱了。
+# 来看看多个线程同时操作一个变量怎么把内容给改乱了：
+# =============================================================================
+balance = 0     # 假定这是你的银行存款:
+def change_it(n):  # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        change_it(n)
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
+
--- 采集爱立信基站断站_修改版.py
+++ 采集爱立信基站断站_修改版.py
@@ -14,13 +14,6 @@ from telnetlib import Telnet
 import socket 
 import pandas as pd
 
-def test_socket_timeout():      #设置和测试socket_timeout的函数
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    print("Default socket timeout: %s" %s.gettimeout())
-    s.settimeout(300)
-    print("Current socket timeout: %s" %s.gettimeout())
-#test_socket_timeout()
-
 
 sche=sched.scheduler(time.time,time.sleep)  # 实例化sched.scheduler类
 # =============================================================================
@@ -29,7 +22,8 @@ sche=sched.scheduler(time.time,time.sleep)  # 实例化sched.scheduler类
 data_path = 'D:\Eric'+'\\'
 out_path = 'D:\Eric'+'\\'
 bts = 'bts_list.xls'
-delay_time = 90 
+delay_time = 90  # 每一批测试命令中间的延时
+
 df_bts = pd.read_excel(data_path + bts , encoding='utf-8')
 ip_list=list(df_bts['IP'])
 command_list=list('ping '+x for x in ip_list)   # 生成ping命令
@@ -70,7 +64,7 @@ def ping_test(n):
             for j in range(i*100,(i+1)*100,1):
                 tn.write(command_list[j].encode('ascii') + b'\n')       # 输入ping命令 
         elif (i+1)*100 > len(command_list):
-            for j in range(i,len(command_list),1):
+            for j in range(i*100,len(command_list),1):
                 tn.write(command_list[j].encode('ascii') + b'\n')       # 输入ping命令 
         tn.write(b'exit'+b'\n')     # 退出telnet服务器     
         time.sleep(delay_time)    
@@ -99,12 +93,12 @@ def task():
     print('任务开始时间:',current_time)   # print任务开始时间
     t0 = datetime.datetime.now()
 
-    input_list = ping_test(list(range(0,10,1)))
+    fault_list = ping_test(list(range(0,10,1)))
     t1 = datetime.datetime.now()
-    if len(input_list) > 0:
+    if len(fault_list) > 0:
         delay_time = delay_time + 90
-        if (t1-t0).seconds <1800:
-            ping_test(input_list)
+        if (t1-t0).seconds <1500:  # 计算程序运行时间，如果小于25分钟，则重测测试失败的部分 
+            ping_test(fault_list)   # 调用ping_test 函数再测一遍测试失败的部分 
     
     current_time = get_current_time()      
     print('任务结束时间：',current_time)
@@ -123,11 +117,13 @@ def task():
     df_break = pd.DataFrame(columns=('eNodeB','基站名称','IP','状态','断站时间','持续时长(分)','恢复时间'))
     df_total = pd.DataFrame(columns=('IP','状态','数据更新时间'))
     
-    current_time = get_current_time()
-    today = current_time.split(' ')[0]
+    today = datetime.date.today()
+    yestoday =  today - datetime.timedelta(1)
+    today = str(today) 
+    yestoday = str(yestoday) 
     for file in all_files:
-        if '测试结果' in file:
-            if file.split(' ')[0] == today:
+        if '_测试结果' in file:
+            if get_time_info(file).split(' ')[0] == today or get_time_info(file).split(' ')[0] == yestoday:
                 file_list.append(file)
             
     if len(file_list) > 0:    # 如果取到的文件列表不为空，则开始处理文件     
@@ -144,81 +140,83 @@ def task():
                     
             success_record = list(x.replace(' is alive','') for x in success_record)
             break_record = list(x.replace('no answer from ','') for x in break_record)    
+            
             df_total_tmp = pd.DataFrame(columns=('IP','状态','数据更新时间'))
+            df_total_tmp2 = pd.DataFrame(columns=('IP','状态','数据更新时间'))
             for i in range(0,len(success_record),1):
                 df_total_tmp.loc[i,'IP'] = success_record[i]
                 df_total_tmp.loc[i,'状态'] = '正常'
                 df_total_tmp.loc[i,'数据更新时间'] = get_time_info(record_file)    
             df_total = df_total.append(df_total_tmp,ignore_index=True)
             for i in range(0,len(break_record),1):
-                df_total_tmp.loc[i,'IP'] = break_record[i]
-                df_total_tmp.loc[i,'状态'] = '断站'
-                df_total_tmp.loc[i,'数据更新时间'] = get_time_info(record_file)
-            df_total = df_total.append(df_total_tmp,ignore_index=True)
-    
-    df_total = df_total.sort_values(by='数据更新时间',ascending = True)    
-        
-    df_break_bts = df_total[df_total['状态'] == '断站']
-    break_bts = list(set(list(df_break_bts['IP'])))  # 通过set去重复，取出所有断站的基站IP的list
-    
-    for i in range(0,len(break_bts),1):
-        df_tmp = df_total[df_total['IP'] == break_bts[i]]  # 筛选该基站的全部记录，包含正常的记录
-        df_tmp = df_tmp.reset_index()
-        break_list = []
-        resume_list = []
-        start_time = df_tmp.loc[0,'数据更新时间']
-        end_time = df_tmp.loc[len(df_tmp)-1,'数据更新时间']
-        if '正常' not in list(df_tmp['状态']):   # 如果记录里都是断站，则断站开始时间等于第一条记录时间
-            break_list.append(start_time)
-        else:
-            for j in range(0,len(df_tmp)-1,1):
-                if df_tmp.loc[j,'状态'] == '断站' and df_tmp.loc[j+1,'状态'] == '正常': # 如果'断站'后面有一行正常状态'正常'则表示故障恢复
-                    resume_list.append(df_tmp.loc[j+1,'数据更新时间'])    # 取最后一行断站记录时间和第一行记录的更新时间的差就是基站断站持续时间    
-                elif df_tmp.loc[j,'状态'] == '正常' and df_tmp.loc[j+1,'状态'] == '断站': # 如果'正常'后面有一行正常状态'断站'则表示发生断站
-                    break_list.append(df_tmp.loc[j+1,'数据更新时间'])    # 取最后一行断站记录时间和第一行记录的更新时间的差就是基站断站持续时间
-            if len(break_list) == 0 and len(resume_list) == 0 :
-                break_list.append(df_tmp.loc[0,'数据更新时间'])   # 如果断站时间和恢复时间都是空，说明记录里全是断站记录，则取第一条作为断站发生时间
-            elif len(break_list) == 0 and len(resume_list) == 1 :     
-                break_list.insert(0,df_tmp.loc[0,'数据更新时间'])   # 如果只有恢复时间没有断站时间,则说明只发生了一次断站，则取第一条记录时间作为断站发生时间  
-            elif len(break_list) > 0 and len(resume_list)> 0: 
-                if time.strptime(resume_list[0],'%Y-%m-%d %H:%M:%S') < time.strptime(break_list[0],'%Y-%m-%d %H:%M:%S'):  
-                    break_list.insert(0,start_time)     #如果第一条恢复时间早于第一条断站时间，则说明还有一次断站发生在更早的时间，取开始时间作为断站时间
-                elif time.strptime(break_list[len(break_list)-1],'%Y-%m-%d %H:%M:%S') > time.strptime(resume_list[len(resume_list)-1],'%Y-%m-%d %H:%M:%S') : 
-                    pass
-    
-        df_break_tmp = pd.DataFrame(columns=('eNodeB','基站名称','IP','状态','断站时间','持续时长(分)','恢复时间'))
-        for j in range(0,len(break_list),1):
-            df_break_tmp.loc[j,'IP'] = df_tmp.loc[0,'IP']
-            df_break_tmp.loc[j,'状态'] = '断站'
-            df_break_tmp.loc[j,'断站时间'] = break_list[j]
-            df_break_tmp = df_break_tmp.fillna('-')
-            if len(resume_list) != 0:
-                df_break_tmp.loc[j,'恢复时间'] = resume_list[j]
-            if df_break_tmp.loc[j,'断站时间'] == '-':
-                df_break_tmp.loc[j,'持续时长(分)'] = (time.mktime(time.strptime(df_break_tmp.loc[j,'恢复时间'],'%Y-%m-%d %H:%M:%S')) - time.mktime(time.strptime(start_time,'%Y-%m-%d %H:%M:%S')))/60
-            elif df_break_tmp.loc[j,'恢复时间'] == '-':
-                df_break_tmp.loc[j,'持续时长(分)'] = (time.mktime(time.strptime(end_time,'%Y-%m-%d %H:%M:%S')) - time.mktime(time.strptime(df_break_tmp.loc[j,'断站时间'],'%Y-%m-%d %H:%M:%S')))/60
+                df_total_tmp2.loc[i,'IP'] = break_record[i]
+                df_total_tmp2.loc[i,'状态'] = '断站'
+                df_total_tmp2.loc[i,'数据更新时间'] = get_time_info(record_file)
+            df_total = df_total.append(df_total_tmp2,ignore_index=True)
+            
+        df_total = df_total.sort_values(by='数据更新时间',ascending = True)    
+            
+        df_break_bts = df_total[df_total['状态'] == '断站']
+        break_bts = list(set(list(df_break_bts['IP'])))  # 通过set去重复，取出所有断站的基站IP的list
+          
+        for i in range(0,len(break_bts),1):
+            df_tmp = df_total[df_total['IP'] == break_bts[i]]  # 筛选该基站的全部记录，包含正常的记录
+            df_tmp = df_tmp.reset_index()
+            break_list = []
+            resume_list = []
+            start_time = df_tmp.loc[0,'数据更新时间']
+            end_time = df_tmp.loc[len(df_tmp)-1,'数据更新时间']
+            if '正常' not in list(df_tmp['状态']):   # 如果记录里都是断站，则断站开始时间等于第一条记录时间
+                break_list.append(start_time)
             else:
-                df_break_tmp.loc[j,'持续时长(分)'] = (time.mktime(time.strptime(df_break_tmp.loc[j,'恢复时间'],'%Y-%m-%d %H:%M:%S')) - time.mktime(time.strptime(df_break_tmp.loc[j,'断站时间'],'%Y-%m-%d %H:%M:%S')))/60
-        df_break =  df_break.append(df_break_tmp,ignore_index=True)
-    
-    
-    df_break['IP'] = df_break['IP'].map(lambda x:x.replace('\n',''))
-    df_break = pd.merge(df_break,df_bts,how='left',on='IP')
-    df_break['eNodeB'] = df_break['eNBId']
-    df_break['基站名称'] = df_break['Site Name(Chinese)']
-    del df_break['eNBId']
-    del df_break['Site Name(Chinese)']
-    
-    current_time = get_current_time() 
-    current_time = current_time.replace(':','.')       
-    writer = pd.ExcelWriter(out_path + current_time + '_断站.xls')
-    df_break.to_excel(writer,current_time + '_断站') 
-    df_total.to_excel(writer,'原始数据') 
-    writer.save()
-    
-    print('报表生成时间：',current_time)
-    print('-----------------------------')
+                for j in range(0,len(df_tmp)-1,1):
+                    if df_tmp.loc[j,'状态'] == '断站' and df_tmp.loc[j+1,'状态'] == '正常': # 如果'断站'后面有一行正常状态'正常'则表示故障恢复
+                        resume_list.append(df_tmp.loc[j+1,'数据更新时间'])    # 取最后一行断站记录时间和第一行记录的更新时间的差就是基站断站持续时间    
+                    elif df_tmp.loc[j,'状态'] == '正常' and df_tmp.loc[j+1,'状态'] == '断站': # 如果'正常'后面有一行正常状态'断站'则表示发生断站
+                        break_list.append(df_tmp.loc[j+1,'数据更新时间'])    # 取最后一行断站记录时间和第一行记录的更新时间的差就是基站断站持续时间
+                if len(break_list) == 0 and len(resume_list) == 0 :
+                    break_list.append(df_tmp.loc[0,'数据更新时间'])   # 如果断站时间和恢复时间都是空，说明记录里全是断站记录，则取第一条作为断站发生时间
+                elif len(break_list) == 0 and len(resume_list) == 1 :     
+                    break_list.insert(0,df_tmp.loc[0,'数据更新时间'])   # 如果只有恢复时间没有断站时间,则说明只发生了一次断站，则取第一条记录时间作为断站发生时间  
+                elif len(break_list) > 0 and len(resume_list)> 0: 
+                    if time.strptime(resume_list[0],'%Y-%m-%d %H:%M:%S') < time.strptime(break_list[0],'%Y-%m-%d %H:%M:%S'):  
+                        break_list.insert(0,start_time)     #如果第一条恢复时间早于第一条断站时间，则说明还有一次断站发生在更早的时间，取开始时间作为断站时间
+                    elif time.strptime(break_list[len(break_list)-1],'%Y-%m-%d %H:%M:%S') > time.strptime(resume_list[len(resume_list)-1],'%Y-%m-%d %H:%M:%S') : 
+                        pass
+        
+            df_break_tmp = pd.DataFrame(columns=('eNodeB','基站名称','IP','状态','断站时间','持续时长(分)','恢复时间'))
+            for j in range(0,len(break_list),1):
+                df_break_tmp.loc[j,'IP'] = df_tmp.loc[0,'IP']
+                df_break_tmp.loc[j,'状态'] = '断站'
+                df_break_tmp.loc[j,'断站时间'] = break_list[j]
+                df_break_tmp = df_break_tmp.fillna('-')
+                if len(resume_list) > j:
+                    df_break_tmp.loc[j,'恢复时间'] = resume_list[j]
+                if df_break_tmp.loc[j,'断站时间'] == '-':
+                    df_break_tmp.loc[j,'持续时长(分)'] = (time.mktime(time.strptime(df_break_tmp.loc[j,'恢复时间'],'%Y-%m-%d %H:%M:%S')) - time.mktime(time.strptime(start_time,'%Y-%m-%d %H:%M:%S')))/60
+                elif df_break_tmp.loc[j,'恢复时间'] == '-':
+                    df_break_tmp.loc[j,'持续时长(分)'] = (time.mktime(time.strptime(end_time,'%Y-%m-%d %H:%M:%S')) - time.mktime(time.strptime(df_break_tmp.loc[j,'断站时间'],'%Y-%m-%d %H:%M:%S')))/60
+                else:
+                    df_break_tmp.loc[j,'持续时长(分)'] = (time.mktime(time.strptime(df_break_tmp.loc[j,'恢复时间'],'%Y-%m-%d %H:%M:%S')) - time.mktime(time.strptime(df_break_tmp.loc[j,'断站时间'],'%Y-%m-%d %H:%M:%S')))/60
+            df_break =  df_break.append(df_break_tmp,ignore_index=True)
+        
+        
+        df_break['IP'] = df_break['IP'].map(lambda x:x.replace('\n',''))
+        df_break = pd.merge(df_break,df_bts,how='left',on='IP')
+        df_break['eNodeB'] = df_break['eNBId']
+        df_break['基站名称'] = df_break['Site Name(Chinese)']
+        del df_break['eNBId']
+        del df_break['Site Name(Chinese)']
+        
+        current_time = get_current_time() 
+        current_time = current_time.replace(':','.')       
+        writer = pd.ExcelWriter(out_path + current_time + '_断站.xls')
+        df_break.to_excel(writer,current_time + '_断站') 
+        #df_total.to_excel(writer,'原始数据') 
+        writer.save()
+        
+        print('报表生成时间：',current_time)
+        print('-----------------------------')
 
 
 
