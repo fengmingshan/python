8ca2ba3a7758d8032a36618b36a2903a3b965335
fengmingshan <fms_python@163.com>
2019-01-19 22:00:39 +0800

    撰写3G邻区自动优化

--- 网优日常/3G邻区自动优化.py
+++ 网优日常/3G邻区自动优化.py
@@ -6,6 +6,7 @@ Created on Fri Jan 18 14:54:34 2019
 """
 
 import pandas as pd 
+import numpy as np
 import os
 import xlwt
 import shutil
@@ -13,7 +14,6 @@ import shutil
 # 设置环境变量
 # =============================================================================
 data_path = r'd:\3G邻区自动优化\BSC1原始数据' + '\\'
-data_path = r'd:\3G邻区自动优化\原始数据' + '\\'
 out_path = r'd:\3G邻区自动优化' + '\\'
 
 # 定义一个文本转xls的函数，因为切换次数的xls文件格式有问题
@@ -64,9 +64,10 @@ for file in cell_config_files:
     df_tmp = pd.read_excel(data_path + file,skiprows = 1,dtype = str ,encoding='utf-8') 
     df_cell_config = df_cell_config.append(df_tmp)
 df_cell_config = df_cell_config[['system','cellid','pilot_pn','alias_b']]
-df_cell_config = df_cell_config.rename(columns = {'pilot_pn':'Scell_pn','pilot_pn':'Scell_name'})
+df_cell_config = df_cell_config.rename(columns = {'pilot_pn':'Scell_pn','alias_b':'Scell_name'})
 df_cell_config['Scell_index'] =  df_cell_config['system'] + '_' + df_cell_config['cellid']
 df_cell_config = df_cell_config[['Scell_index','Scell_name','Scell_pn']]
+
 # 重新获取格式转换后的文件名
 handover_files = [x for x in os.listdir(data_path) if '小区切换邻区对象' in x and '.txt' not in x ] 
 # 合并处理切换次数文件    
@@ -75,18 +76,25 @@ for file in handover_files:
     df_tmp = pd.read_excel(data_path + file,skiprows = 3,encoding='utf-8') 
     df_handover = df_handover.append(df_tmp)
 
-df_handover.columns = df_handover.columns.map(lambda x:x.replace('\n',''))  
-df_handover = pd.pri                                
-df_handover['源BTS标识'] =  df_handover['源BTS标识'].astype(str)
-df_handover['源小区号'] =  df_handover['源小区号'].astype(str)
-df_handover['目标BTS标识'] =  df_handover['目标BTS标识'].astype(str)
-df_handover['目标小区号'] =  df_handover['目标小区号'].astype(str)
+df_handover.columns = df_handover.columns.map(lambda x:x.replace('\n',''))
+df_handover['源BTS标识'] = df_handover['源BTS标识'].astype(str)
+df_handover['源小区号'] = df_handover['源小区号'].astype(str) 
+df_handover['目标BTS标识'] = df_handover['目标BTS标识'].astype(str) 
+df_handover['目标小区号'] = df_handover['目标小区号'].astype(str)  
+df_handover['Scell_index'] = df_handover['源BTS标识'] + '_' + df_handover['源小区号']
 df_handover['neighbor_index'] = df_handover['源BTS标识'] + '_' + df_handover['源小区号'] + \
                                 '-' + df_handover['目标BTS标识'] + '_' + df_handover['目标小区号']
 df_handover['切换总次数'] = df_handover['切换成功次数'] + df_handover['无效导频失败次数'] + df_handover['拥塞失败次数'] + df_handover['其他失败次数']                              
+df_handover = df_handover[['neighbor_index','Scell_index','切换总次数','切换成功次数']]
+df_handover = pd.pivot_table(df_handover, index=['neighbor_index','Scell_index'], 
+                              values =['切换总次数' ,'切换成功次数'], 
+                              aggfunc = {'切换总次数':np.sum,'切换成功次数':np.sum}) 
+df_handover = df_handover.reset_index() 
+df_handover = pd.merge(df_handover,df_cell_config,how = 'left', on = 'Scell_index')
+df_handover = df_handover[['neighbor_index','Scell_index','Scell_name','Scell_pn','切换总次数','切换成功次数']]
 df_handover['切换成功率(%)'] =  df_handover['切换成功次数']/df_handover['切换总次数']
-df_handover = df_handover[['neighbor_index','切换总次数','切换成功次数','切换成功率(%)']]
 
+ 
 # 合并处理小区邻区文件            
 df_cell_neighbor = pd.DataFrame()
 for file in cell_neighbor_files:
@@ -118,5 +126,12 @@ df_carrier_neighbor = pd.merge(df_carrier_neighbor,df_handover,how ='left', on =
 # =============================================================================
 # 检查小区邻区
 # =============================================================================
-df_cell = df_cell_neighbor[['system','cellid','neighbor_pn','ncellsystemid','ncellid','alias_b','neighbor_index']]
+df_cell = df_cell_neighbor[['system','cellid','ncellsystemid','ncellid','neighbor_pn','neighbor_name','neighbor_index']]
 df_cell_check = pd.merge(df_handover,df_cell,how = 'left',on = 'neighbor_index')
+df_cell_check = df_cell_check[['system','cellid','Scell_index','Scell_name','Scell_pn','ncellsystemid',\
+                               'ncellid','neighbor_name','neighbor_pn','切换总次数',\
+                               '切换成功次数','切换成功率(%)','neighbor_index']]
+
+df_cell_check = df_cell_check.sort_values(by='切换总次数',ascending = False) 
+#with pd.ExcelWriter(out_path + 'cell_check.xlsx') as writer: #不用保存和退出，系统自动会完成
+#    df_cell_check.to_excel(writer,'cell_check',index = False) 
