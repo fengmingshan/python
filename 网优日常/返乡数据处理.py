e1da11477b24538624e98ad64aef75c647c7b86d
fengmingshan <fms_python@163.com>
2019-02-02 13:37:05 +0800

    Update 返乡数据处理.py

--- 网优日常/返乡数据处理.py
+++ 网优日常/返乡数据处理.py
@@ -20,6 +20,9 @@ file2 = '曲靖号段.xls'
 支局数据2 = '爱立信关联支局.xlsx'
 
 df_tmp = pd.read_csv(data_path + file1 ,sep=';',encoding='gbk',engine = 'python',dtype = str) 
+for col in df_tmp.columns:
+    df_tmp[col] = df_tmp[col].map(lambda x:x.replace('nan',''))
+    
 df_号段 = pd.read_excel(data_path + file2 ,encoding='utf-8') 
 df_支局1 = pd.read_excel(data_path + 支局数据1 ,encoding='utf-8') 
 df_支局1 = df_支局1[['网元','区县','乡镇_街道']]
@@ -36,16 +39,22 @@ df_tmp = pd.merge(df_tmp,df_号段,how = 'left' , on = '前7位' )
 df_tmp = df_tmp[df_tmp['曲靖本地号段'] != '曲靖']
 
 df_tmp = df_tmp.sort_values(by=['手机号','总流量'],ascending = False) # 按时间顺序升序排列  
-df_tmp.drop_duplicates(subset='手机号', keep='first', inplace=True)
+df_tmp.drop_duplicates('手机号', keep='first', inplace=True)
+df_tmp.drop_duplicates()
+
 df_tmp = df_tmp.reset_index()
 df_tmp.drop('index',axis = 1,inplace = True)
 df_tmp['网元'] = df_tmp['小区'].map(lambda x:x.split('_')[0]) 
 df_tmp['网元'] = df_tmp['网元'].astype(int)
 df_tmp = pd.merge(df_tmp,df_支局,how = 'left' , on = '网元' )
+df_tmp = df_tmp[['区县','乡镇_街道','小区','手机号','号码归属省','号码归属市','总流量','终端品牌','终端型号','日期','号码归属省','号码归属市','上周常驻城市','上周常驻区域']]
+df_tmp.rename(columns={'小区':'基站名称','终端品牌':'手机品牌','终端型号':'手机型号'},inplace =True)
+df_tmp.fillna('-',inplace = True)
+
 区县列表 = list(set(df_tmp['区县']))
 df_区县 = pd.DataFrame()
 for 区县 in 区县列表:
     df_区县 = df_tmp[df_tmp['区县'] == 区县]  
     with pd.ExcelWriter(out_path + 区县 + '_返乡数据.xlsx') as writer: #不用保存和退出，系统自动会完成
-        df_区县.to_excel(writer, 区县 + '返乡数据') 
+        df_区县.to_excel(writer, 区县 + '返乡数据',index = False,) 
 
