d61ad34fdf79f8a843ba1039caacd457bd64ffc9
fengmingshan <fms_python@163.com>
2019-04-11 17:11:03 +0800

    完善！

--- 网优日常/4A系统保持在线_360浏览器.py
+++ 网优日常/4A系统保持在线_360浏览器.py
@@ -15,19 +15,29 @@ import time
 __browser_url = r'C:\Users\Administrator\AppData\Roaming\360se6\Application\360se.exe' 
 #登录页面
 url = r'http://135.32.99.19'
-url1 = r'http://www.baidu.com'
+
 
 chrome_options = Options()
 chrome_options.binary_location = __browser_url
 
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get(url1)
+driver.get(url)
+username = driver.find_element_by_name('login_name')
+username.click()
+username.send_keys(15334434064)
+username.send_keys(Keys.ENTER)
+password = driver.find_element_by_name('password')
+password.click()
+password.send_keys('Ab12345678')
+driver.find_element_by_link_text('获取短信密码').click()
+driver.find_element_by_name('smsPassword').click()
+
 
 # 等待一定时间，让js脚本加载完毕
 driver.implicitly_wait(30)
 try:
     while True :
-        time.sleep(60)
+        time.sleep(300)
         driver.refresh()
 except KeyboardInterrupt:
     print('检测到CTRL+C,准备退出程序!') 
--- 网优日常/4A系统保持在线_IE浏览器.py
+++ 网优日常/4A系统保持在线_IE浏览器.py
@@ -18,16 +18,27 @@ url = r'http://135.32.99.19'
 
 driver=webdriver.Ie()
 driver.get(url)
+username = driver.find_element_by_name('login_name')
+username.click()
+username.send_keys(15334434064)
+username.send_keys(Keys.ENTER)
+password = driver.find_element_by_name('password')
+password.click()
+password.send_keys('Ab12345678')
+driver.find_element_by_link_text('获取短信密码').click()
+time.sleep(3)
+driver.switch_to_alert().accept()  # 针对IE提出的提示信息点击重试按钮
+driver.find_element_by_name('smsPassword').click()
 
 # 等待一定时间，让js脚本加载完毕
 driver.implicitly_wait(30)
 try:
     while True :
-        time.sleep(120)
+        time.sleep(30)
         driver.refresh()
+        driver.switch_to_alert().accept()  # 针对IE提出的提示信息点击重试按钮
         current_time = str(datetime.today()).split('.')[0]
-        print('4A系统保持在线! : ' + current_time) 
-
+        print("4A系统保持在线! : " + current_time)          
 except KeyboardInterrupt:
     print('检测到CTRL+C,准备退出程序!') 
     driver.close()
