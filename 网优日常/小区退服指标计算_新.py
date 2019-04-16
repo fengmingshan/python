7736a33bfaba8e91fb11a12fefb8ddca0b88c26a
fengmingshan <fms_python@163.com>
2019-04-16 11:43:57 +0800

    增加文件：小区退服指标计算_新

---
+++ 网优日常/小区退服指标计算_新.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 09:39:32 2019
+
+@author: Administrator
+"""
+
+import pandas as pd 
+import os
+from datetime import datetime 
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False # 用来正常显示负号
+
+current_date = str(datetime.now()).split('.')[0].split(' ')[0]
+
+data_path = r'D:\2019年工作\2019年4月小区退服指标计算（新）' + '\\'
+pic_path = r'D:\2019年工作\2019年4月小区退服指标计算（新）\PIC' + '\\'
+
+all_files = os.listdir(data_path)
+files = [x for x in all_files if 'alarm_cel_exit_service_child' in x] 
+
+df_原始数据 = pd.DataFrame()
+for file in files :
+    df_tmp = pd.read_excel(data_path + file,skiprows = 1)
+    df_原始数据 = df_原始数据.append(df_tmp)
+
+# =============================================================================
+# 计算各县累计断站时长
+# =============================================================================
+
+with  pd.ExcelWriter(data_path + '断站原始数据.xlsx')  as writer:  #输出到excel
+    df_原始数据.to_excel(writer,'断站原始数据',index=False) 
+
+    
---
+++ 网优日常/批量打开MR测量.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 16:32:20 2019
+
+@author: Administrator
+"""
+
+import pandas as pd
+import os
+from datetime import datetime 
+current_date = str(datetime.now()).split('.')[0].split(' ')[0]
+
+data_path = r'D:\test' + '\\'
+
+df_OMMB1 = pd.read_excel(data_path + 'EUtranCellMeasurement_OMMB1.xlsx',encoding='utf-8')
+df_OMMB1 = df_OMMB1.drop(0)
+df_OMMB1 = df_OMMB1.drop(1)
+df_OMMB1 = df_OMMB1.drop(2)
+df_OMMB1 =  df_OMMB1[df_OMMB1['intraFPeriodMeasSwitch'] == '0']
+df_OMMB1 =  df_OMMB1.reset_index()  
+
+df_OMMB2 = pd.read_excel(data_path + 'EUtranCellMeasurement_OMMB2.xlsx',encoding='utf-8')
+df_OMMB2 = df_OMMB2.drop(0)
+df_OMMB2 = df_OMMB2.drop(1)
+df_OMMB2 = df_OMMB2.drop(2)
+df_OMMB2 =  df_OMMB2[df_OMMB2['intraFPeriodMeasSwitch'] == '0']
+df_OMMB2 =  df_OMMB2.reset_index()  
+
+df_OMMB3 = pd.read_excel(data_path + 'EUtranCellMeasurement_OMMB3.xlsx',encoding='utf-8')
+df_OMMB3 = df_OMMB3.drop(0)
+df_OMMB3 = df_OMMB3.drop(1)
+df_OMMB3 = df_OMMB3.drop(2)
+df_OMMB3 =  df_OMMB3[df_OMMB3['intraFPeriodMeasSwitch'] == '0']
+df_OMMB3 =  df_OMMB3.reset_index()  
+
+
+
+with open(data_path + current_date+ '_' +  'Modify_MR_OMMB1.txt','w') as f:
+    for i in range(0,len(df_OMMB1),1):
+        line = r'UPDATE:MOC="EUtranCellMeasurement",MOI="{0}",ATTRIBUTES="intraFPeriodMeasSwitch=1",EXTENDS="";'\
+        .format(df_OMMB1.loc[i,'MOI'])
+        f.write(line+'\n')         
+   
+with open(data_path + 'Apply_right_MR_OMMB1.txt','w') as f:
+    for i in range(0,len(df_OMMB1),1):
+        line = r'APPLY MUTEXRIGHT:SUBNET="{0}",NE="{1}";'\
+        .format(df_OMMB1.loc[i,'SubNetwork'],
+                df_OMMB1.loc[i,'MEID']
+)
+        f.write(line+'\n') 
+
+with open(data_path + current_date + '_' +  'Modify_MR_OMMB2.txt','w') as f:
+    for i in range(0,len(df_OMMB2),1):
+        line = r'UPDATE:MOC="EUtranCellMeasurement",MOI="{0}",ATTRIBUTES="intraFPeriodMeasSwitch=1",EXTENDS="";'\
+        .format(df_OMMB2.loc[i,'MOI'])
+        f.write(line+'\n')         
+   
+with open(data_path  + 'Apply_right_MR_OMMB2.txt','w') as f:
+    for i in range(0,len(df_OMMB2),1):
+        line = r'APPLY MUTEXRIGHT:SUBNET="{0}",NE="{1}";'\
+        .format(df_OMMB2.loc[i,'SubNetwork'],
+                df_OMMB2.loc[i,'MEID']
+)
+        f.write(line+'\n') 
+
+with open(data_path + current_date + '_' +  'Modify_MR_OMMB3.txt','w') as f:
+    for i in range(0,len(df_OMMB3),1):
+        line = r'UPDATE:MOC="EUtranCellMeasurement",MOI="{0}",ATTRIBUTES="intraFPeriodMeasSwitch=1",EXTENDS="";'\
+        .format(df_OMMB3.loc[i,'MOI'])
+        f.write(line+'\n')         
+   
+with open(data_path  + 'Apply_right_MR_OMMB3.txt','w') as f:
+    for i in range(0,len(df_OMMB3),1):
+        line = r'APPLY MUTEXRIGHT:SUBNET="{0}",NE="{1}";'\
+        .format(df_OMMB3.loc[i,'SubNetwork'],
+                df_OMMB3.loc[i,'MEID']
+)
+        f.write(line+'\n') 
+
+#with  pd.ExcelWriter(path + 'df_top.xlsx')  as writer:  #输出到excel
+    #df_top.to_excel(writer, 'df_top') 
+       
+        
