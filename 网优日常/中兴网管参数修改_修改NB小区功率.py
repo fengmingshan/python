916e29556f2186ef4c3eba6f4a175a996bb6eae9
fengmingshan <fms_python@163.com>
2019-04-19 16:33:33 +0800

    Update 中兴网管参数修改_修改NB小区功率.py

--- 网优日常/中兴网管参数修改_修改NB小区功率.py
+++ 网优日常/中兴网管参数修改_修改NB小区功率.py
@@ -22,12 +22,15 @@ file_ommb3 = 'ECellEquipmentFunctionNB_OMMB3.xlsx'
 # =============================================================================
 df_ommb1 = pd.read_excel(data_path + file_ommb1 ,encoding='utf-8') 
 df_ommb1['MOI'] = df_ommb1['MOI'].map(lambda x:x.replace('ConfigSet=0,',''))
+df_ommb1_eNode = df_ommb1[['SubNetwork','MEID']]
+df_ommb1_eNode.drop_duplicates('MEID', 'first' ,inplace = True)
+df_ommb1_eNode = df_ommb1_eNode.reset_index()
 
 with open(data_path + current_date + '_' + 'apply_right_OMMB1.txt','a') as f:
-    for i in range(0,len(df_ommb1),1):
+    for i in range(0,len(df_ommb1_eNode),1):
         line = r'APPLY MUTEXRIGHT:SUBNET="{0}",NE="{1}";'\
-        .format(df_ommb1.loc[i,'SubNetwork'],
-                df_ommb1.loc[i,'MEID'],
+        .format(df_ommb1_eNode.loc[i,'SubNetwork'],
+                df_ommb1_eNode.loc[i,'MEID'],
 )
         f.write(line+'\n')         
 
@@ -43,12 +46,16 @@ with open(data_path + current_date + '_'  + 'OMMB1_chang_NBCellPower.txt','a') a
 # =============================================================================
 df_ommb2 = pd.read_excel(data_path + file_ommb2 ,encoding='utf-8') 
 df_ommb2['MOI'] = df_ommb2['MOI'].map(lambda x:x.replace('ConfigSet=0,',''))
+df_ommb2_eNode = df_ommb2[['SubNetwork','MEID']]
+df_ommb2_eNode.drop_duplicates('MEID', 'first' ,inplace = True)
+df_ommb2_eNode = df_ommb2_eNode.reset_index()
+
 
 with open(data_path + current_date + '_'  + 'apply_right_OMMB2.txt','a') as f:
-    for i in range(0,len(df_ommb2),1):
+    for i in range(0,len(df_ommb2_eNode),1):
         line = r'APPLY MUTEXRIGHT:SUBNET="{0}",NE="{1}";'\
-        .format(df_ommb2.loc[i,'SubNetwork'],
-                df_ommb2.loc[i,'MEID'],
+        .format(df_ommb2_eNode.loc[i,'SubNetwork'],
+                df_ommb2_eNode.loc[i,'MEID'],
 )
         f.write(line+'\n')         
 
@@ -64,12 +71,15 @@ with open(data_path + current_date + '_'  + 'OMMB2_chang_NBCellPower.txt','a') a
 # =============================================================================
 df_ommb3 = pd.read_excel(data_path + file_ommb3 ,encoding='utf-8') 
 df_ommb3['MOI'] = df_ommb3['MOI'].map(lambda x:x.replace('ConfigSet=0,',''))
+df_ommb3_eNode = df_ommb3[['SubNetwork','MEID']]
+df_ommb3_eNode.drop_duplicates('MEID', 'first' ,inplace = True)
+df_ommb3_eNode = df_ommb3_eNode.reset_index()
 
 with open(data_path + current_date + '_'  + 'apply_right_OMMB3.txt','a') as f:
-    for i in range(0,len(df_ommb3),1):
+    for i in range(0,len(df_ommb3_eNode),1):
         line = r'APPLY MUTEXRIGHT:SUBNET="{0}",NE="{1}";'\
-        .format(df_ommb3.loc[i,'SubNetwork'],
-                df_ommb3.loc[i,'MEID'],
+        .format(df_ommb3_eNode.loc[i,'SubNetwork'],
+                df_ommb3_eNode.loc[i,'MEID'],
 )
         f.write(line+'\n')         
 
