1ae721494d796b4385da78e8ddbea3b059734ab4
fengmingshan <fms_python@163.com>
2019-04-19 16:20:35 +0800

    Update 中兴网管参数修改_修改NB小区功率.py

--- 网优日常/中兴网管参数修改_修改NB小区功率.py
+++ 网优日常/中兴网管参数修改_修改NB小区功率.py
@@ -10,17 +10,18 @@ import os
 from datetime import datetime 
 current_date = str(datetime.now()).split('.')[0].split(' ')[0]
 
-data_path = r'D:\test' + '\\'
+data_path = r'd:\2019年工作\2019年4月NB小区关断节能' + '\\'
 
-file_name = '修改NB小区功率_2019-04-19.xlsx'
-df_file = pd.read_excel(data_path + file_name ,encoding='utf-8') 
+file_ommb1 = 'ECellEquipmentFunctionNB_OMMB1.xlsx'
+file_ommb2 = 'ECellEquipmentFunctionNB_OMMB2.xlsx'
+file_ommb3 = 'ECellEquipmentFunctionNB_OMMB3.xlsx'
+
 
 # =============================================================================
 # 生成ommb1修改指令
 # =============================================================================
-df_ommb1 = df_file[df_file['OMMB'] == 'OMMB1']
-df_ommb1 = df_ommb1.reset_index()
-
+df_ommb1 = pd.read_excel(data_path + file_ommb1 ,encoding='utf-8') 
+df_ommb1['MOI'] = df_ommb1['MOI'].map(lambda x:x.replace('ConfigSet=0,',''))
 
 with open(data_path + current_date + '_' + 'apply_right_OMMB1.txt','a') as f:
     for i in range(0,len(df_ommb1),1):
@@ -31,18 +32,17 @@ with open(data_path + current_date + '_' + 'apply_right_OMMB1.txt','a') as f:
         f.write(line+'\n')         
 
 
-with open(data_path + current_date + '_'  + 'OMMB1_command.txt','a') as f:
-    for i in range(0,len(df_ommb1),1):
-        line = r'UPDATE:MOC="EUtranCellFDD",MOI="{0}",ATTRIBUTES="flagSwiMode=6",EXTENDS="";'\
+with open(data_path + current_date + '_'  + 'OMMB1_chang_NBCellPower.txt','a') as f:
+    for i in range(0,len(df_ommb1),1):                                                           
+        line = r'UPDATE:MOC="ECellEquipmentFunctionNB",MOI="{0}",ATTRIBUTES="cpTransPwr=10.0",EXTENDS="";'\
         .format(df_ommb1.loc[i,'MOI'])
         f.write(line+'\n') 
 
 # =============================================================================
-# 生成ommb1修改指令
+# 生成ommb2修改指令
 # =============================================================================
-df_ommb2 = df_file[df_file['OMMB'] == 'OMMB2']
-df_ommb2 = df_ommb2.reset_index()
-
+df_ommb2 = pd.read_excel(data_path + file_ommb2 ,encoding='utf-8') 
+df_ommb2['MOI'] = df_ommb2['MOI'].map(lambda x:x.replace('ConfigSet=0,',''))
 
 with open(data_path + current_date + '_'  + 'apply_right_OMMB2.txt','a') as f:
     for i in range(0,len(df_ommb2),1):
@@ -53,8 +53,29 @@ with open(data_path + current_date + '_'  + 'apply_right_OMMB2.txt','a') as f:
         f.write(line+'\n')         
 
 
-with open(data_path + current_date + '_'  + 'OMMB2_command.txt','a') as f:
+with open(data_path + current_date + '_'  + 'OMMB2_chang_NBCellPower.txt','a') as f:
     for i in range(0,len(df_ommb2),1):
-        line = r'UPDATE:MOC="EUtranCellFDD",MOI="{0}",ATTRIBUTES="flagSwiMode=6",EXTENDS="";'\
-        .format(df_ommb2.loc[i,'MOI'])
+        line = r'UPDATE:MOC="ECellEquipmentFunctionNB",MOI="{0}",ATTRIBUTES="cpTransPwr=10.0",EXTENDS="";'\
+        .format(df_ommb2.loc[i,'MOI'],)
+        f.write(line+'\n') 
+
+# =============================================================================
+# 生成ommb3修改指令
+# =============================================================================
+df_ommb3 = pd.read_excel(data_path + file_ommb3 ,encoding='utf-8') 
+df_ommb3['MOI'] = df_ommb3['MOI'].map(lambda x:x.replace('ConfigSet=0,',''))
+
+with open(data_path + current_date + '_'  + 'apply_right_OMMB3.txt','a') as f:
+    for i in range(0,len(df_ommb3),1):
+        line = r'APPLY MUTEXRIGHT:SUBNET="{0}",NE="{1}";'\
+        .format(df_ommb3.loc[i,'SubNetwork'],
+                df_ommb3.loc[i,'MEID'],
+)
+        f.write(line+'\n')         
+
+
+with open(data_path + current_date + '_'  + 'OMMB3_chang_NBCellPower.txt','a') as f:
+    for i in range(0,len(df_ommb3),1):
+        line = r'UPDATE:MOC="ECellEquipmentFunctionNB",MOI="{0}",ATTRIBUTES="cpTransPwr=10.0",EXTENDS="";'\
+        .format(df_ommb3.loc[i,'MOI'])
         f.write(line+'\n') 
