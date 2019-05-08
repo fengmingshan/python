d87ca106f11520f95ad59033296ae2b5b5b5e37b
fengmingshan <fms_python@163.com>
2019-05-08 09:05:28 +0800

    Update 遍历java源代码_找出import的类.py

--- 遍历java源代码_找出import的类.py
+++ 遍历java源代码_找出import的类.py
@@ -22,13 +22,15 @@ with open(out_path + 'java文件列表.txt' ,'w') as writer:
 
 line_list = []    
 for file_name in java_file:
-    file_content = open(file_name,encoding = 'utf-8').readlines()
-    for line in file_content:
-        if  'import ' in line: 
-            line_list.append(line.replace('\n','') + ' ' + file_name + '\n')
+    with open(file_name,encoding = 'utf-8') as file_content:
+        for line in file_content.readlines():
+            if  '42.99.18.27:9777' in line: 
+                line_list.append(line.replace('\n','') + ' ' + file_name + '\n')
 
-with open(out_path + 'import库列表.txt' ,'w') as writer:
+with open(out_path + '感知采集.txt' ,'w') as writer:
     for line in line_list:
         writer.writelines(line)
+
+
             
             
