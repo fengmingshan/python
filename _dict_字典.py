2d1c75511f7649fb0662b8a2623b79b204fcfccd
fengmingshan <fms_python@163.com>
2019-05-29 11:41:31 +0800

    完成邻区规划

--- _dict_字典.py
+++ _dict_字典.py
@@ -4,7 +4,7 @@ Created on Tue Dec 19 16:18:06 2017
 字典_数据结构:由键值对组成（键和对应的值，键是一个KEY,是不允许重复的，但是值可以重复），是无序的
 @author: Administrator
 """
-import pandas as pd 
+import pandas as pd
 
 dict1 = {'Alice': '2341', 'Beth': '9102', 'Cecil': '3258'}
 dict2 = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
@@ -30,15 +30,15 @@ s='Name'
 dict3[s]
 
 #字典的初始化
-d={}  
-s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]  
+d={}
+s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]
 
-for i,j in s:  
-    d[i]=j  
+for i,j in s:
+    d[i]=j
 print(d)
 for i in d.keys():
-    print(i) 
-    
+    print(i)
+
 # =============================================================================
 # 进阶字典与map联合使用
 # =============================================================================
@@ -81,32 +81,42 @@ for k, v in d.items():
 
 
 #字典的初始化
-k={}  
-s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]  
-for i,j in s:  
-    if i not in k.keys():#注意不能写成 if not k[i],因为其返回值不是None,而是error  
-        l=[]  
-        l.append(j)  
-        k[i]=l  
-    else:  
-        k[i].append(j)  
-print(k)  
+k={}
+s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]
+for i,j in s:
+    if i not in k.keys():#注意不能写成 if not k[i],因为其返回值不是None,而是error
+        l=[]
+        l.append(j)
+        k[i]=l
+    else:
+        k[i].append(j)
+print(k)
 
 
 #字典的初始化
-from collections import defaultdict  
-d=defaultdict(list)  
-s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]  
-for i,j in s:  
-    d[i].append(j)  
-print(list(d.items()))  
+from collections import defaultdict
+d=defaultdict(list)
+s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]
+for i,j in s:
+    d[i].append(j)
+print(list(d.items()))
 
-from collections import defaultdict  
-d=defaultdict(set)  
-s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]  
-for i,j in s:  
-    d[i].add(j)  
-print(list(d.items()))  
+from collections import defaultdict
+d=defaultdict(set)
+s = [('Tom', 5), ('Jone', 2), ('Susan', 4), ('Tom', 4), ('Tom', 1)]
+for i,j in s:
+    d[i].add(j)
+print(list(d.items()))
+
+# 字典的初始化，一键多值的情况
+dic4 = {}
+key = 1
+value_arr = [0,1,2,3,4]
+value_arr1 = [5,6,7,8,9]
+dic4.setdefault(key, []).append(value_arr)
+dic4.setdefault(key, []).append(value_arr1)
+print ( value_arr[4],' ',dic4[1][0][4])
+print ( value_arr1[0],' ',dic4[1][1][0])
 
 #字典的操作获取所有key
 info = dict(name='cold', blog='linuxzen.com')
@@ -118,5 +128,5 @@ x = list(info.values())
 info = dict(name='cold', blog='linuxzen.com')
 for key, value in info.items():
     print key, ':',  value
-    
+
 
--- 网优日常/邻区规划_根据经纬度绘制Voronoi图.py
+++ 网优日常/邻区规划_根据经纬度绘制Voronoi图.py
@@ -14,7 +14,11 @@ from scipy.spatial import voronoi_plot_2d
 import matplotlib.pyplot as plt
 
 data_path ='D:\_邻区自动规划' + '\\'
+zte_bts800 = '曲靖电信LTE工参(L800M)20190425.xls'
+zte_bts1800 = '曲靖电信LTE工参(L1.8G)20190425.xls'
+zte_eric800 ='爱立信云南曲靖电信工参表20190428.xlsx'
 cell_file = '全网小区_合并.csv'
+
 
 def millerToXY (lon, lat):
      xy_coordinate = [] # x，y坐标集
@@ -36,36 +40,56 @@ def millerToXY (lon, lat):
      xy_coordinate.append(int(round(y,8)))
      return xy_coordinate # 装换后的 x,y 坐标
 
-def lon2x(lon)
-     '''
-     经度转换为平面坐标系中的x 利用米勒坐标系
-     :param lon: 经度
-     '''
-     L = 6381372*math.pi*2
-     W = L
-     H = L/2
-     mill = 2.3
-     x = lon*math.pi/180
-     x = (W/2)+(W/(2*math.pi))*x
-     return round(x,5)
+df_tmp = pd.read_excel(data_path + zte_bts800)
+df_zte800 =pd.DataFrame(columns = ['eNodeB_ID','Cell_name','Cell_index','LON','LAT','azimuth','network','manufacturers'])
+df_zte800['eNodeB_ID'] = df_tmp['ENODEBID']
+df_zte800['Cell_name'] = df_tmp['CELLNAME']
+name = df_tmp['ENODEBID'].map(str)  + '_' + df_tmp['CELLNAME'].map(lambda x:x.split('_')[2])
+df_zte800['name'] = name
+df_zte800['Cell_index'] = df_tmp['ENODEBID'].map(str)  + df_tmp['CELLID'].map(str)
+df_zte800['LON'] = df_tmp['LONC']
+df_zte800['LAT'] = df_tmp['LATC']
+df_zte800['azimuth'] = df_tmp['Azimuth']
+df_zte800['network'] = 'L800'
+df_zte800['manufacturers'] = 'ZTE'
 
-def lat2y (lat):
-     '''
-     纬度转换为平面坐标系中的y 利用米勒坐标系
-     :param lat: 维度
-     '''
-     L = 6381372*math.pi*2
-     W = L
-     H = L/2
-     mill = 2.3
-     y = lat*math.pi/180
-     y = 1.25*math.log(math.tan(0.25*math.pi+0.4*y))
-     y = (H/2)-(H/(2*mill))*y
-     return round(y,5)
+df_tmp = pd.read_excel(data_path + zte_bts1800)
+df_zte1800 =pd.DataFrame(columns = ['eNodeB_ID','Cell_name','Cell_index','LON','LAT','azimuth','network','manufacturers'])
+df_zte1800['eNodeB_ID'] = df_tmp['ENODEBID']
+df_zte1800['Cell_name'] = df_tmp['CELLNAME']
+df_zte1800['name'] = df_tmp['CELLNAME'].map(lambda x:x.split('_')[2])
+name = df_tmp['ENODEBID'].map(str)  + '_' + df_tmp['CELLNAME'].map(lambda x:x.split('_')[2])
+df_zte1800['name'] = name
+df_zte1800['Cell_index'] = df_tmp['ENODEBID'].map(str)  + df_tmp['CELLID'].map(str)
+df_zte1800['LON'] = df_tmp['LONC']
+df_zte1800['LAT'] = df_tmp['LATC']
+df_zte1800['azimuth'] = df_tmp['Azimuth']
+df_zte1800['network'] = 'L1.8'
+df_zte1800['manufacturers'] = 'ZTE'
 
+df_tmp = pd.read_excel(data_path + zte_eric800)
+df_eric800 =pd.DataFrame(columns = ['eNodeB_ID','Cell_name','Cell_index','LON','LAT','azimuth','network','manufacturers'])
+df_eric800['eNodeB_ID'] = df_tmp['eNBId']
+df_eric800['Cell_name'] = df_tmp['Cell_name']
+name = df_tmp['eNBId'].map(str)  + '_' + df_tmp['Cell_name'].map(lambda x:x.split('_')[0])
+df_eric800['name'] = name
+df_eric800['Cell_index'] =  df_tmp['Cell_index']
+df_eric800['LON'] = df_tmp['longitude']
+df_eric800['LAT'] = df_tmp['latitude']
+df_eric800['azimuth'] = df_tmp['azimuth']
+df_eric800['network'] = df_tmp['channelBandwidth'].map({'5M':'L800M', '15M':'L1.8'})
+df_eric800['manufacturers'] = 'ERIC'
 
-df_cell = pd.read_csv(data_path + cell_file,engine = 'python' )
-df_cell.drop_duplicates('eNodeB_ID', keep='first', inplace = True)
+df_all_cells = df_zte800.append(df_zte1800).append(df_eric800)
+df_all_cells = df_all_cells.reset_index()
+df_all_cells.drop('index',axis =1,inplace =True)
+with open(data_path + '全网小区汇总.csv','w') as  writer:
+     df_all_cells.to_csv(writer,index = False)
+
+#df_all_cells = pd.read_csv(data_path + 'test.csv',engine = 'python')
+df_cell = df_all_cells.drop_duplicates('name', keep='first')
+df_cell = df_cell[~df_cell['Cell_name'].str.contains('室分') ]
+df = pd.DataFrame({"A": ["Hello", "
 df_cell['xy_coordinate'] = df_cell.apply(lambda x:millerToXY(x.LON , x.LAT) , axis = 1 )
 df_cell = df_cell.reset_index()
 df_cell.drop('index',axis =1,inplace = True)
@@ -74,6 +98,8 @@ point_list = [x for x in df_cell['xy_coordinate']]
 points = np.array(point_list)
 # 计算Voronoi图
 vor = Voronoi(points=points)
+voronoi_plot_2d(vor)
+plt.show()
 
 seed_ponts = vor.points
 df_cell['point_index'] = list(df_cell.index)
@@ -87,44 +113,52 @@ df_regions['regions_index'] = range(len(regions))
 df_regions['regions'] = regions
 df_cell = pd.merge(df_cell,df_regions,on = 'regions_index',how = 'left' )
 
-
 # =============================================================================
 # 规划第一层邻区
 # =============================================================================
-layer1_neighbor_dict = dict()
+layer1_neighbor_name_dict = dict()
+layer1_neighbor_index_dict = dict()
 layer1_regions_dict = dict()
-for i in range(3):
-     neighbors_layer1 = []
+for i in range(10):
+     neighbors_name_layer1 = []
+     neighbors_index_layer1 = []
      regions_layer1 = []
      cell_name = df_cell.loc[i,'Cell_name']
+     cell_index = df_cell.loc[i,'Cell_index']
      cell_region = df_cell.loc[i,'regions']
      for j in range(0,len(df_cell)):
           if len(set(cell_region) & set(df_cell.loc[j,'regions'])) > 0 \
-               and df_cell.loc[j,'regions'] != cell_region:
-                    neighbors_layer1.append(df_cell.loc[j,'Cell_name'])
+               and df_cell.loc[j,'Cell_name'] != cell_name:
+                    neighbors_name_layer1.append(df_cell.loc[j,'Cell_name'])
+                    neighbors_index_layer1.append(df_cell.loc[j,'Cell_index'])
                     regions_layer1.append(df_cell.loc[j,'regions'])
-     layer1_neighbor_dict[cells_name]= neighbors_layer1
-     layer1_regions_dict[cells_region] = regions_layer1
+     layer1_neighbor_name_dict[cell_name]= neighbors_name_layer1
+     layer1_neighbor_index_dict[cell_index] = neighbors_index_layer1
+     layer1_regions_dict[cell_index] = regions_layer1
 
 # =============================================================================
 # 规划第二层邻区
 # =============================================================================
-layer2_neighbor_dict = dict()
+layer2_neighbor_name_dict = dict()
+layer2_neighbor_index_dict = dict()
 layer2_regions_dict = dict()
-for i in range(3):
+for i in range(10):
+     neighbors_name_layer2 = []
+     neighbors_index_layer2 = []
      cell_name = df_cell.loc[i,'Cell_name']
-     cell_region = df_cell.loc[i,'regions']
-     neighbors_layer_2 = []
-     regions_layer_2 = []
-     for layer1_region in layer1_regions_dict[cell_region]:
+     cell_index = df_cell.loc[i,'Cell_index']
+     for layer1_region in layer1_regions_dict[cell_index]:
           for j in range(0,len(df_cell)):
                if len(set(layer1_region) & set(df_cell.loc[j,'regions'])) > 0 \
-                    and df_cell.loc[j,'regions'] != layer1_region:
-                         neighbors_layer_2.append(df_cell.loc[j,'Cell_name'])
-                         regions_layer_2.append(df_cell.loc[j,'regions'])
-     layer2_neighbor_dict[cells_name]= neighbors_layer_2
-     layer2_regions_dict[cells_region] = regions_layer_2
+                    and df_cell.loc[j,'regions'] not in layer1_regions_dict[cell_index]\
+                    and df_cell.loc[j,'regions'] != df_cell.loc[i,'regions']:
+                         neighbors_name_layer2.append(df_cell.loc[j,'Cell_name'])
+                         neighbors_index_layer2.append(df_cell.loc[j,'Cell_index'])
+     layer2_neighbor_name_dict[cell_name]= list(set(neighbors_name_layer2))
+     layer2_neighbor_index_dict[cell_index] =  list(set(neighbors_index_layer2))
 
+#with open(data_path + '全网layer规划.xlsx','w') as  writer:
+#     df_all_cells.to_csv(writer,index = False)
 
 # max_bound: 种子点的最大边界
 # min_bound: 种子点的最小边界
