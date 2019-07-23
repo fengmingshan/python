6a5f3796e97f9c7a1e9015a61f8b7f03594de8c0
fengmingshan <fms_python@163.com>
2019-07-23 11:40:45 +0800

    Update 超忙小区分析_中兴.py

--- 网优日常/超忙小区分析_中兴.py
+++ 网优日常/超忙小区分析_中兴.py
@@ -27,6 +27,7 @@ df_zte['小区总流量(GB)'] = round(((df_zte['空口上行用户面流量（MB
                          + df_zte['空口下行用户面流量（MByte）_1477070755617-11'])/1024),2)
 df_zte['日期'] = df_zte['开始时间'].map(lambda x:str(x).split(' ')[0])
 df_zte['Net_type'] = df_zte['小区名称'].map(lambda x:x.split('_')[1])
+df_zte['Net_type'] = df_zte['Net_type'].astype(int)
 
 df_zte_L1800 = df_zte[df_zte['Net_type'].isin(L1800_list)]
 
@@ -52,8 +53,8 @@ df_busy_cell_count_1800 = pd.pivot_table(df_pivot_1800, index=['网元','小区'
 df_busy_cell_count_1800  = df_busy_cell_count_1800.reset_index()
 
 
-df_high_Data_800 = df_zte_L800[(df_zte_L800['小区总流量(GB)'] >= 6) & (df_zte_L800['下行PRB平均占用率_1'] >= 0.5) ]
-df_massive_users_800 = df_zte_L800[(df_zte_L800['最大RRC连接用户数_1'] >= 200) & (df_zte_L800['下行PRB平均占用率_1'] >= 0.5) ]
+df_high_Data_800 = df_zte_L800[(df_zte_L800['小区总流量(GB)'] >= 1.5) & (df_zte_L800['下行PRB平均占用率_1'] >= 0.5) ]
+df_massive_users_800 = df_zte_L800[(df_zte_L800['最大RRC连接用户数_1'] >= 50) & (df_zte_L800['下行PRB平均占用率_1'] >= 0.5) ]
 
 df_busy_cell_800 = df_high_Data_800.append(df_massive_users_800)
 
@@ -71,13 +72,34 @@ df_busy_cell_count_800 = pd.pivot_table(df_pivot_800, index=['网元','小区','
                                                       aggfunc = {'日期':'count'})
 df_busy_cell_count_800  = df_busy_cell_count_800.reset_index()
 
+df_busy_cell_count_1800['小区名称'] = df_busy_cell_count_1800['小区名称'].map(lambda x:x.replace('曲靖','QJ'))
+df_busy_cell_count_1800['小区名称'] = df_busy_cell_count_1800['小区名称'].map(lambda x:x.replace('Q麒麟','QJ麒麟'))
+df_busy_cell_count_1800['小区名称'] = df_busy_cell_count_1800['小区名称'].map(lambda x:x.replace('QJQJ','QJ'))
 
-df_busy_cell_count_1800['小区名称']
+df_busy_cell_count_1800['区县'] = df_busy_cell_count_1800['小区名称'].map(lambda x:x.split('QJ')[1][:2])
+
+df1 = df_busy_cell_count_1800[df_busy_cell_count_1800['区县'] == '']
+
+df_country_1800 = pd.pivot_table(df_busy_cell_count_1800, index=['区县'],
+                                                           values =['小区名称'],
+                                                           aggfunc = {'小区名称':'count'})
+
+
+df_busy_cell_count_800['小区名称'] = df_busy_cell_count_800['小区名称'].map(lambda x:x.replace('曲靖','QJ'))
+df_busy_cell_count_800['区县'] = df_busy_cell_count_800['小区名称'].map(lambda x:x.split('QJ')[1][:2])
+
+df_country_800 = pd.pivot_table(df_busy_cell_count_800, index=['区县'],
+                                                           values =['小区名称'],
+                                                           aggfunc = {'小区名称':'count'})
+
 
 
 with  pd.ExcelWriter(data_path + '中兴超忙小区.xlsx')  as writer:  #输出到excel
     df_busy_cell_count_1800.to_excel(writer,'1800超忙小区',index=False)
+    df_country_1800.to_excel(writer,'1800超忙按县统计',index=False)
     df_busy_cell_count_800.to_excel(writer,'L800超忙小区',index=False)
+    df_country_800.to_excel(writer,'800M超忙按县统计',index=False)
+
 
 
 
