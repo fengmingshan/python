68cc204d8ab224a56d2a7b9ed5af80aefc9b5e95
fengmingshan <fms_python@163.com>
2019-08-20 16:15:34 +0800

    零星修改

--- 网优日常/超忙小区分析_爱立信.py
+++ 网优日常/超忙小区分析_爱立信.py
@@ -4,7 +4,12 @@ Created on Tue Aug 20 15:06:25 2019
 
 @author: Administrator
 """
+import pandas as pd
+import os
+import numpy as np
 
+L1800_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,49, 50, 51, 52, 53, 54, 55, 56, 129, 130, 131, 132, 133, 134, 135, 136,177, 178, 179, 180, 181, 182]
+L800_list = [17, 18, 19, 20, 21, 22,145, 146, 147, 148, 149,150]
 title_list = ['DATE_ID',
               'HOUR_ID',
               'eNodeB',
@@ -20,7 +25,7 @@ title_list = ['DATE_ID',
               'Avg Number of UL Active Users', 'Avg Number of DL Active Users',
               'Avg User Fell Throughput (Mbps)']
 
-print(title_list)
+data_path = r'd:\_小程序\超忙小区分析' + '\\'
 
-title1 = ['DATE_ID', 'HOUR_ID', 'eNodeB', 'EUTRANCELLFDD', 'Acc_Wireless ConnSucRate(%)', 'Acc_ERAB_dropping rate (%)', 'Air Interface_Traffic_Volume_UL_MBytes', 'Air Interface_Traffic_Volume_DL_MBytes', 'Int_Downlink Latency (ms)', 'Max number of UE in RRc', 'DL_Util_of_PRB', 'pmCellDowntimeAuto1', 'pmCellDowntimeMan1', 'Data_Coverage', 'Ava_CellAvail (%)', 'Num of LTE Redirect to 3G', 'Avg Number of UL Active Users', 'Avg Number of DL Active Users', 'Avg User Fell Throughput (Mbps)']
+
 
