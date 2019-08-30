a4413e7e288a7ca04b0637eaacf5c6feea7710cd
fengmingshan <fms_python@163.com>
2019-08-30 17:14:57 +0800

    零星修改

--- 网优日常/LTE新开站邻区规划_voronoi图.py
+++ 网优日常/LTE新开站邻区规划_voronoi图.py
@@ -4,7 +4,6 @@ Created on Wed Aug 28 10:32:43 2019
 
 @author: Administrator
 """
-
 import pandas as pd
 import os
 import math
@@ -26,28 +25,30 @@ from math import atan2
 from math import atan
 from math import ceil
 
-data_path ='D:/_小程序/新开站邻区规划/'
+data_path = 'D:/_小程序/新开站邻区规划/'
 cell_file = '西城规划扇区.xlsx'
 
-def millerToXY (lon, lat):
-     xy_coordinate = [] # x，y坐标集
-     '''
+
+def millerToXY(lon, lat):
+    xy_coordinate = []  # x，y坐标集
+    '''
      经纬度转换为平面坐标系中的x,y 利用米勒坐标系
      :param lon: 经度
      :param lat: 维度
      '''
-     L = 6381372*math.pi*2
-     W = L
-     H = L/2
-     mill = 2.3
-     x = lon*math.pi/180
-     y = lat*math.pi/180
-     y = 1.25*math.log(math.tan(0.25*math.pi+0.4*y))
-     x = (W/2)+(W/(2*math.pi))*x
-     y = (H/2)-(H/(2*mill))*y
-     xy_coordinate.append(int(round(x,8)))
-     xy_coordinate.append(int(round(y,8)))
-     return xy_coordinate # 装换后的 x,y 坐标
+    L = 6381372 * math.pi * 2
+    W = L
+    H = L / 2
+    mill = 2.3
+    x = lon * math.pi / 180
+    y = lat * math.pi / 180
+    y = 1.25 * math.log(math.tan(0.25 * math.pi + 0.4 * y))
+    x = (W / 2) + (W / (2 * math.pi)) * x
+    y = (H / 2) - (H / (2 * mill)) * y
+    xy_coordinate.append(int(round(x, 8)))
+    xy_coordinate.append(int(round(y, 8)))
+    return xy_coordinate  # 装换后的 x,y 坐标
+
 
 def clac_degree(latA, lonA, latB, lonB):
     """
@@ -69,27 +70,30 @@ def clac_degree(latA, lonA, latB, lonB):
     brng = (brng + 360) % 360
     return brng
 
-def calc_azimuth_difference(Degree,Scell_azimuth,Ncell_azimuth):
-     '''计算目标小区是否要添加为邻区
-        根据距离和方位角判断：
-        当基站距离小于2km时，源小区和目标基站所有小区都要添加为邻区;
-        当基站距离大于2km小于5km时，只有源小区正对目标小区的扇区和目标基站所有小区添加邻区;
-        当基站距离大于5km时，只有源小区正对目标小区的扇区和目标基站正对源小区的小区添加邻区;'''
-     Scell_degree_detal = abs(Scell_azimuth - Degree) if abs(Scell_azimuth - Degree) < 180 \
-                    else 360 - abs(Scell_azimuth - Degree)
-     neighbor_Degree_detal = abs(Ncell_azimuth - Degree) if abs(Ncell_azimuth - Degree) < 180 \
-                              else 360 - abs(Ncell_azimuth - Degree)
-     if Scell_degree_detal + neighbor_Degree_detal <= 90:
-          add_neighbor ='Yes'
-     else:
-          add_neighbor ='No'
-     return add_neighbor
+
+def calc_azimuth_difference(Degree, Scell_azimuth, Ncell_azimuth):
+    '''计算目标小区是否要添加为邻区
+       根据距离和方位角判断：
+       当基站距离小于2km时，源小区和目标基站所有小区都要添加为邻区;
+       当基站距离大于2km小于5km时，只有源小区正对目标小区的扇区和目标基站所有小区添加邻区;
+       当基站距离大于5km时，只有源小区正对目标小区的扇区和目标基站正对源小区的小区添加邻区;'''
+    Scell_degree_detal = abs(Scell_azimuth - Degree) if abs(Scell_azimuth - Degree) < 180 \
+        else 360 - abs(Scell_azimuth - Degree)
+    neighbor_Degree_detal = abs(Ncell_azimuth - Degree) if abs(Ncell_azimuth - Degree) < 180 \
+        else 360 - abs(Ncell_azimuth - Degree)
+    if Scell_degree_detal + neighbor_Degree_detal <= 90:
+        add_neighbor = 'Yes'
+    else:
+        add_neighbor = 'No'
+    return add_neighbor
+
 
 df_cell = pd.read_excel(data_path + cell_file)
 
-df_bts = df_cell[['ENODEBName','LONB','LATB']]
-df_bts.drop_duplicates('ENODEBName',keep = 'first',inplace = True)
-df_bts['xy_coordinate'] = df_bts.apply(lambda x:millerToXY(x.LONB , x.LATB) , axis = 1 )
+df_bts = df_cell[['ENODEBName', 'LONB', 'LATB']]
+df_bts.drop_duplicates('ENODEBName', keep='first', inplace=True)
+df_bts['xy_coordinate'] = df_bts.apply(
+    lambda x: millerToXY(x.LONB, x.LATB), axis=1)
 # 初始化种子点
 point_list = [x for x in df_bts['xy_coordinate']]
 points = np.array(point_list)
@@ -110,18 +114,19 @@ regions = vor.regions
 df_regions = pd.DataFrame()
 df_regions['regions_index'] = range(len(regions))
 df_regions['regions'] = regions
-df_bts = pd.merge(df_bts,df_regions,on = 'regions_index',how = 'left' )
+df_bts = pd.merge(df_bts, df_regions, on='regions_index', how='left')
 
-df_bts_regions = df_bts[['ENODEBName','regions']]
-df_bts_regions.set_index('ENODEBName',inplace = True)
+df_bts_regions = df_bts[['ENODEBName', 'regions']]
+df_bts_regions.set_index('ENODEBName', inplace=True)
 dict_regions = df_bts_regions['regions'].to_dict()
 df_cell['regions'] = df_cell['ENODEBName'].map(dict_regions)
 plan_bts_name = '麒麟西城师范学院樱苑二栋L8'
+
 # =============================================================================
-# 规划第一层邻区
+# 规划邻区
 # =============================================================================
-@jit
-def plan_first_layer(plan_bts_name,df_cell):
+@jit 
+def plan_first_layer(plan_bts_name, df_cell):
     df_cell['Scell'] = ''
     df_cell['first_layer'] = ''
     df_cell['second_layer'] = ''
@@ -130,45 +135,50 @@ def plan_first_layer(plan_bts_name,df_cell):
     df_plan_cell = df_cell[df_cell['ENODEBName'] == plan_bts_name]
     plan_cell = list(set(df_plan_cell['CELLNAME']))
     cell_region = set(list(df_plan_cell['regions'])[0])
-    for i in range(0,len(df_cell)):
-        if len(cell_region & set(df_cell.loc[i,'regions'])) > 0 \
-            and df_cell.loc[i,'CELLNAME'] != plan_cell:
-            df_cell.loc[i,'first_layer'] = 1
+    for i in range(0, len(df_cell)):
+        if len(cell_region & set(df_cell.loc[i, 'regions'])) > 0 \
+                and df_cell.loc[i, 'CELLNAME'] != plan_cell:
+            df_cell.loc[i, 'first_layer'] = 1
     # 筛选出第一层邻区清单
     df_first_layer = df_cell[df_cell['first_layer'] == 1]
     df_first = pd.DataFrame()
-    for i in range(0,len(plan_cell)):
-        if len(plan_cell) -1-i >= 0 :
+    for i in range(0, len(plan_cell)):
+        if len(plan_cell) - 1 - i >= 0:
             df_tmp = df_first_layer.copy()
-            df_tmp['Scell'] = plan_cell[len(plan_cell)-i-1]
-            df_first = df_first.append(df_tmp,ignore_index=True)
+            df_tmp['Scell'] = plan_cell[len(plan_cell) - i - 1]
+            df_first = df_first.append(df_tmp, ignore_index=True)
 
     # 计算第一层邻区及第一层邻区的regions
     first_layer_neighbor = list(df_first_layer['CELLNAME'])
-    first_layer_regions = set(reduce(operator.concat, list(df_first_layer['regions'])))
+    first_layer_regions = set(
+        reduce(operator.concat, list(df_first_layer['regions'])))
     S_Lon = list(df_plan_cell['LONB'])[0]
     S_Lat = list(df_plan_cell['LATB'])[0]
     S_Azimuth = list(df_plan_cell['Azimuth'])[0]
     df_second = pd.DataFrame()
     for cell in plan_cell:
-        for i in range(0,len(df_cell)):
-            if len(first_layer_regions & set(df_cell.loc[i,'regions'])) > 0 \
-                and df_cell.loc[i,'CELLNAME'] !=  cell \
-                and df_cell.loc[i,'CELLNAME'] not in  first_layer_neighbor:
-                df_cell.loc[i,'second_layer'] = 1
-                df_cell.loc[i,'Scell'] = cell
-                df_cell.loc[i,'degree'] = clac_degree(S_Lon,S_Lat,df_cell.loc[i,'LONB'],df_cell.loc[i,'LATB'])
-                df_cell['opposite'] = calc_azimuth_difference(df_cell.loc[i,'degree'],S_Azimuth,df_cell.loc[i,'Azimuth'])
-            else :
-                df_cell.loc[i,'second_layer'] = 0
-            df_second_layer = df_cell[(df_cell['second_layer'] == 1)&(df_cell['opposite'] == 'Yes')]
+        for i in range(0, len(df_cell)):
+            if len(first_layer_regions & set(df_cell.loc[i, 'regions'])) > 0 \
+                    and df_cell.loc[i, 'CELLNAME'] != cell \
+                    and df_cell.loc[i, 'CELLNAME'] not in first_layer_neighbor:
+                df_cell.loc[i, 'second_layer'] = 1
+                df_cell.loc[i, 'Scell'] = cell
+                df_cell.loc[i, 'degree'] = clac_degree(
+                    S_Lon, S_Lat, df_cell.loc[i, 'LONB'], df_cell.loc[i, 'LATB'])
+                df_cell['opposite'] = calc_azimuth_difference(
+                    df_cell.loc[i, 'degree'], S_Azimuth, df_cell.loc[i, 'Azimuth'])
+            else:
+                df_cell.loc[i, 'second_layer'] = 0
+            df_second_layer = df_cell[(df_cell['second_layer'] == 1) & (
+                df_cell['opposite'] == 'Yes')]
             df_second = df_second.append(df_second_layer)
-    df_planed = df_first.append(df_second,ignore_index=True)
+    df_planed = df_first.append(df_second, ignore_index=True)
     return df_planed
 
-df_planed = plan_first_layer('麒麟西城师范学院樱苑二栋L8',df_cell)
-df_planed = df_planed[['Scell','CELLNAME','first_layer','second_layer']]
-df_planed.rename(columns = {'CELLNAME':'Ncell'},inplace = True)
-df_planed.drop_duplicates(['Scell','Ncell'],keep = 'first',inplace = True)
+
+df_planed = plan_first_layer('麒麟西城师范学院樱苑二栋L8', df_cell)
+df_planed = df_planed[['Scell', 'CELLNAME', 'first_layer', 'second_layer']]
+df_planed.rename(columns={'CELLNAME': 'Ncell'}, inplace=True)
+df_planed.drop_duplicates(['Scell', 'Ncell'], keep='first', inplace=True)
 with pd.ExcelWriter(data_path + '邻区规划结果.xlsx') as writer:
-    df_planed.to_excel(writer,index = False)
+    df_planed.to_excel(writer, index=False)
--- 网优日常/处理网优平台1X通话数据.py
+++ 网优日常/处理网优平台1X通话数据.py
@@ -11,40 +11,42 @@ import numpy as np
 from numba import jit
 
 
-def judge_uesr_type(voice,data,avg_data):
-     user_info_list = []
-     if (voice == 0)&(data == 0):
-          uesr_type = '双零用户'
-          uesr_level = '无'
-     elif (voice < 10)& (data > 0) & (avg_data <30):
-          uesr_type = '数据卡用户'
-          uesr_level = '轻度'
-     elif (voice < 10) & (data > 0) & (avg_data >=30):
-          uesr_type = '数据卡用户'
-          uesr_level = '重度'
-     elif (voice < 10) & (data >= 0) & (data < 31457280):
-          uesr_type = '纯语音用户'
-          uesr_level = '无'
-     elif (voice >= 10) & (data >= 0) & (data < 31457280):
-          uesr_type = '纯语音用户'
-          uesr_level = '无'
-     elif (voice >= 10) & (data > 31457280) & (avg_data <30):
-          uesr_type = '语音数据用户'
-          uesr_level = '轻度'
-     elif (voice >= 10) & (data > 31457280) & (avg_data >=30):
-          uesr_type = '语音数据用户'
-          uesr_level = '重度'
-     else :
-          uesr_type = '否'
-          uesr_level = '否'
-     user_info_list = [uesr_type,uesr_level]
-     return user_info_list
+def judge_uesr_type(voice, data, avg_data):
+    user_info_list = []
+    if (voice == 0) & (data == 0):
+        uesr_type = '双零用户'
+        uesr_level = '无'
+    elif (voice < 10) & (data > 0) & (avg_data < 30):
+        uesr_type = '数据卡用户'
+        uesr_level = '轻度'
+    elif (voice < 10) & (data > 0) & (avg_data >= 30):
+        uesr_type = '数据卡用户'
+        uesr_level = '重度'
+    elif (voice < 10) & (data >= 0) & (data < 31457280):
+        uesr_type = '纯语音用户'
+        uesr_level = '无'
+    elif (voice >= 10) & (data >= 0) & (data < 31457280):
+        uesr_type = '纯语音用户'
+        uesr_level = '无'
+    elif (voice >= 10) & (data > 31457280) & (avg_data < 30):
+        uesr_type = '语音数据用户'
+        uesr_level = '轻度'
+    elif (voice >= 10) & (data > 31457280) & (avg_data >= 30):
+        uesr_type = '语音数据用户'
+        uesr_level = '重度'
+    else:
+        uesr_type = '否'
+        uesr_level = '否'
+    user_info_list = [uesr_type, uesr_level]
+    return user_info_list
+
 
 out_path = r'd:\2019年工作\2019年8月4G网络扩频方案\结果输出' + '\\'
 
 call_file_1X = r'd:\2019年工作\2019年8月4G网络扩频方案\网优平台导出1X通话数据\小区下面用户通话次数(不分释放原因).csv'
-df_1X_call = pd.read_csv(call_file_1X ,engine = 'python')
-df_1X_call = df_1X_call[(df_1X_call['cell_name'].str.contains('QJ'))&(~df_1X_call['cell_name'].str.contains('六螺蛳'))]
+df_1X_call = pd.read_csv(call_file_1X, engine='python')
+df_1X_call = df_1X_call[(df_1X_call['cell_name'].str.contains(
+    'QJ')) & (~df_1X_call['cell_name'].str.contains('六螺蛳'))]
 df_1X_call['imsi'] = df_1X_call['imsi'].astype(str)
 call_user_set = set(df_1X_call['imsi'])
 
@@ -54,194 +56,204 @@ abnormal_user = '移动号码非正常.csv'
 state_file = '移动用户状态字典.xlsx'
 
 df_state = pd.read_excel(billing_path + state_file)
-df_state.set_index('状态码' , inplace =True )
+df_state.set_index('状态码', inplace=True)
 state_dict = df_state['状态'].to_dict()
 
 number_imsi_file = r'd:\2019年工作\2019年8月4G网络扩频方案\HLR导出数据\number_imsi.csv'
-df_number_imsi = pd.read_csv(number_imsi_file,engine ='python')
+df_number_imsi = pd.read_csv(number_imsi_file, engine='python')
 df_number_imsi['IMSI'] = df_number_imsi['IMSI'].astype(str)
-df_number_imsi.set_index('IMSI',inplace = True)
+df_number_imsi.set_index('IMSI', inplace=True)
 imsi_number_dict1 = df_number_imsi['号码'].to_dict()
 
 number_imsi_file2 = r'd:\2019年工作\2019年8月4G网络扩频方案\网优平台导出1X通话数据\imsi.csv'
-df_number_imsi2 = pd.read_csv(number_imsi_file2,engine ='python')
-df_number_imsi2 = df_number_imsi2.sort_values(by=['数据生成时间','ms_num'],ascending = False)
-df_number_imsi2.drop_duplicates('ms_num', keep='first', inplace = True)
+df_number_imsi2 = pd.read_csv(number_imsi_file2, engine='python')
+df_number_imsi2 = df_number_imsi2.sort_values(
+    by=['数据生成时间', 'ms_num'], ascending=False)
+df_number_imsi2.drop_duplicates('ms_num', keep='first', inplace=True)
 df_number_imsi2['imsi'] = df_number_imsi2['imsi'].astype(str)
-df_number_imsi2.set_index('imsi',inplace = True)
+df_number_imsi2.set_index('imsi', inplace=True)
 imsi_number_dict2 = df_number_imsi2['ms_num'].to_dict()
-for k,v in imsi_number_dict2.items():
-     if k not in imsi_number_dict1.items():
-          imsi_number_dict1[k] = v
+for k, v in imsi_number_dict2.items():
+    if k not in imsi_number_dict1.items():
+        imsi_number_dict1[k] = v
 
 
-df_normal = pd.read_csv(billing_path + normal_user,engine = 'python',dtype =str )
-df_abnormal = pd.read_csv(billing_path + abnormal_user,engine = 'python',dtype =str )
-df_normal['IMSI码'] = df_normal['IMSI码'].str.replace('nan','-')
-df_abnormal['IMSI码'] = df_abnormal['IMSI码'].str.replace('nan','-')
+df_normal = pd.read_csv(billing_path + normal_user, engine='python', dtype=str)
+df_abnormal = pd.read_csv(billing_path + abnormal_user,
+                          engine='python', dtype=str)
+df_normal['IMSI码'] = df_normal['IMSI码'].str.replace('nan', '-')
+df_abnormal['IMSI码'] = df_abnormal['IMSI码'].str.replace('nan', '-')
 
 df_normal['状态'] = df_normal['STATE'].map(state_dict)
 df_abnormal['状态'] = df_abnormal['STATE'].map(state_dict)
-df_valid_user  = df_abnormal[~df_abnormal['状态'].isin(['用户申请拆机(移动业务)','用户要求停机','双停','挂失'])]
+df_valid_user = df_abnormal[~df_abnormal['状态'].isin(
+    ['用户申请拆机(移动业务)', '用户要求停机', '双停', '挂失'])]
 df_normal_user = df_normal.append(df_valid_user)
 
-df_user_state = df_normal_user[df_normal_user['IMSI码'] != '-'][['IMSI码','状态']]
+df_user_state = df_normal_user[df_normal_user['IMSI码'] != '-'][['IMSI码', '状态']]
 df_user_state['IMSI码'] = df_user_state['IMSI码'].astype(str)
-df_user_state.set_index('IMSI码' , inplace =True )
+df_user_state.set_index('IMSI码', inplace=True)
 user_state_dict = df_user_state['状态'].to_dict()
 
-df_1X_call['区县'] = df_1X_call['cell_name'].map(lambda x:x.split('QJ')[1][:2])
-df_1X_call['乡镇'] = df_1X_call['cell_name'].map(lambda x:x.split('QJ')[1][2:4])
+df_1X_call['区县'] = df_1X_call['cell_name'].map(lambda x: x.split('QJ')[1][:2])
+df_1X_call['乡镇'] = df_1X_call['cell_name'].map(lambda x: x.split('QJ')[1][2:4])
 
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('十八','十八连山'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('黄泥','黄泥河'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('大莫','大莫古'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('小百','小百户'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('三岔','三岔河'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('八大','八大河'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('大水','大水井'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('旧屋','旧屋基'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('鲁布','鲁布革'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('王家','王家庄'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('马过','马过河'))
-df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x:x.replace('白石','白石江'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('十八', '十八连山'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('黄泥', '黄泥河'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('大莫', '大莫古'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('小百', '小百户'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('三岔', '三岔河'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('八大', '八大河'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('大水', '大水井'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('旧屋', '旧屋基'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('鲁布', '鲁布革'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('王家', '王家庄'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('马过', '马过河'))
+df_1X_call['乡镇'] = df_1X_call['乡镇'].map(lambda x: x.replace('白石', '白石江'))
 
 df_1X_call['用户状态'] = df_1X_call['imsi'].map(user_state_dict)
 df_1X_call['用户号码'] = df_1X_call['imsi'].map(imsi_number_dict1)
 df_1X_call = df_1X_call[df_1X_call['用户号码'].notnull()]
 
 df_number_pivot = pd.pivot_table(df_1X_call, index=['用户号码'],
-                                            values =['通话次数'],
-                                            aggfunc = {'通话次数':np.sum})
+                                 values=['通话次数'],
+                                 aggfunc={'通话次数': np.sum})
 number_calls_dict = df_number_pivot['通话次数'].to_dict()
 
 df_1X_call['无线接入次数'] = df_1X_call['用户号码'].map(number_calls_dict)
 
 df_1X_call_pivot = pd.pivot_table(df_1X_call,
-                                  index=['用户号码','imsi','用户状态','区县','乡镇','无线接入次数'],
-                                  values =['通话次数'],
-                                  aggfunc = {'通话次数':np.sum})
-df_1X_call_pivot.reset_index(inplace = True)
-df_1X_call_pivot['常驻地占比'] = round(df_1X_call_pivot['通话次数']/df_1X_call_pivot['无线接入次数'],4)
-df_1X_call_pivot.rename(columns={'通话次数':'连接次数'
-                                 },inplace =True)
+                                  index=['用户号码', 'imsi', '用户状态',
+                                         '区县', '乡镇', '无线接入次数'],
+                                  values=['通话次数'],
+                                  aggfunc={'通话次数': np.sum})
+df_1X_call_pivot.reset_index(inplace=True)
+df_1X_call_pivot['常驻地占比'] = round(
+    df_1X_call_pivot['通话次数']/df_1X_call_pivot['无线接入次数'], 4)
+df_1X_call_pivot.rename(columns={'通话次数': '连接次数'
+                                 }, inplace=True)
 
 
 @jit()
 def calc_user_home(df):
-     user_number_set = set(df['用户号码'])
-     df_uesr_home_all = pd.DataFrame()
-     Append = pd.DataFrame.append
-     i = 0
-     for number in user_number_set:
-          i += 1
-          df_user_calls = df[df['用户号码'] == number]
-          max_call_number = df_user_calls['连接次数'].max()
-          df_uesr_home_all = Append(df_uesr_home_all,df_user_calls[df_user_calls['连接次数'] == max_call_number])
-          if i%100 == 0:
-               print('finished: ', i ,' total: ', len(user_number_set))
-     return df_uesr_home_all
+    user_number_set = set(df['用户号码'])
+    df_uesr_home_all = pd.DataFrame()
+    Append = pd.DataFrame.append
+    i = 0
+    for number in user_number_set:
+        i += 1
+        df_user_calls = df[df['用户号码'] == number]
+        max_call_number = df_user_calls['连接次数'].max()
+        df_uesr_home_all = Append(
+            df_uesr_home_all, df_user_calls[df_user_calls['连接次数'] == max_call_number])
+        if i % 100 == 0:
+            print('finished: ', i, ' total: ', len(user_number_set))
+    return df_uesr_home_all
 
-df_uesr_home_all  =  calc_user_home(df_1X_call_pivot)
+
+df_uesr_home_all = calc_user_home(df_1X_call_pivot)
 
 user_data_traffic = r'd:\2019年工作\2019年8月4G网络扩频方案\计费系统导出\划小清单201907.csv'
-df_user_data_traffic = pd.read_csv(user_data_traffic,engine = 'python')
-df_user_data_traffic = df_user_data_traffic[~df_user_data_traffic['用户号码'].map(lambda x:str(x)[:4] == '0874' or str(x)[:3] == '0874')]
+df_user_data_traffic = pd.read_csv(user_data_traffic, engine='python')
+df_user_data_traffic = df_user_data_traffic[~df_user_data_traffic['用户号码'].map(
+    lambda x:str(x)[:4] == '0874' or str(x)[:3] == '0874')]
 df_user_data_traffic['用户号码'] = df_user_data_traffic['用户号码'].astype(np.int64)
-df_user_data_traffic['日均流量_MB'] = round(df_user_data_traffic['上网流量']/(1024*1024*30),2)
-df_user_data_traffic['平均网速_KBps'] = round(df_user_data_traffic['上网流量']/df_user_data_traffic['上网时长'],0)
+df_user_data_traffic['日均流量_MB'] = round(
+    df_user_data_traffic['上网流量']/(1024*1024*30), 2)
+df_user_data_traffic['平均网速_KBps'] = round(
+    df_user_data_traffic['上网流量']/df_user_data_traffic['上网时长']/1024, 0)
 
-df_user_data_traffic['user_info'] =  df_user_data_traffic.apply(lambda x:judge_uesr_type(x.通话次数,x.上网流量,x.日均流量_MB),axis = 1)
-df_user_data_traffic['user_type'] = df_user_data_traffic['user_info'].map(lambda x:x[0])
-df_user_data_traffic['user_level'] = df_user_data_traffic['user_info'].map(lambda x:x[1])
+df_user_data_traffic['user_info'] = df_user_data_traffic.apply(
+    lambda x: judge_uesr_type(x.通话次数, x.上网流量, x.日均流量_MB), axis=1)
+df_user_data_traffic['user_type'] = df_user_data_traffic['user_info'].map(
+    lambda x: x[0])
+df_user_data_traffic['user_level'] = df_user_data_traffic['user_info'].map(
+    lambda x: x[1])
 
-df_user_type = df_user_data_traffic[['用户号码','user_type']]
-df_user_type.set_index('用户号码',inplace =True)
+df_user_type = df_user_data_traffic[['用户号码', 'user_type']]
+df_user_type.set_index('用户号码', inplace=True)
 user_type_dict = df_user_type['user_type'].to_dict()
 
-df_user_level = df_user_data_traffic[['用户号码','user_level']]
-df_user_level.set_index('用户号码',inplace =True)
+df_user_level = df_user_data_traffic[['用户号码', 'user_level']]
+df_user_level.set_index('用户号码', inplace=True)
 user_level_dict = df_user_level['user_level'].to_dict()
 
-df_user_call_count = df_user_data_traffic[['用户号码','通话次数']]
-df_user_call_count.set_index('用户号码',inplace =True)
+df_user_call_count = df_user_data_traffic[['用户号码', '通话次数']]
+df_user_call_count.set_index('用户号码', inplace=True)
 user_call_count_dict = df_user_call_count['通话次数'].to_dict()
 
-df_user_call_druation = df_user_data_traffic[['用户号码','通话时长']]
-df_user_call_druation.set_index('用户号码',inplace =True)
+df_user_call_druation = df_user_data_traffic[['用户号码', '通话时长']]
+df_user_call_druation.set_index('用户号码', inplace=True)
 user_call_druation_dict = df_user_call_druation['通话时长'].to_dict()
 
-df_user_data = df_user_data_traffic[['用户号码','上网流量']]
-df_user_data.set_index('用户号码',inplace =True)
+df_user_data = df_user_data_traffic[['用户号码', '上网流量']]
+df_user_data.set_index('用户号码', inplace=True)
 user_data_dict = df_user_data['上网流量'].to_dict()
 
-df_user_avg_data = df_user_data_traffic[['用户号码','日均流量_MB']]
-df_user_avg_data.set_index('用户号码',inplace =True)
+df_user_avg_data = df_user_data_traffic[['用户号码', '日均流量_MB']]
+df_user_avg_data.set_index('用户号码', inplace=True)
 user_avg_data_dict = df_user_avg_data['日均流量_MB'].to_dict()
 
-df_user_duration = df_user_data_traffic[['用户号码','上网时长']]
-df_user_duration.set_index('用户号码',inplace =True)
+df_user_duration = df_user_data_traffic[['用户号码', '上网时长']]
+df_user_duration.set_index('用户号码', inplace=True)
 user_duration_dict = df_user_duration['上网时长'].to_dict()
 
-df_user_times = df_user_data_traffic[['用户号码','上网次数']]
-df_user_times.set_index('用户号码',inplace =True)
+df_user_times = df_user_data_traffic[['用户号码', '上网次数']]
+df_user_times.set_index('用户号码', inplace=True)
 user_times_dict = df_user_times['上网次数'].to_dict()
 
-df_user_speed = df_user_data_traffic[['用户号码','平均网速_KBps']]
-df_user_speed.set_index('用户号码',inplace =True)
+df_user_speed = df_user_data_traffic[['用户号码', '平均网速_KBps']]
+df_user_speed.set_index('用户号码', inplace=True)
 user_speed_dict = df_user_speed['平均网速_KBps'].to_dict()
 
 df_uesr_home_all['用户号码'] = df_uesr_home_all['用户号码'].astype(np.int64)
 df_uesr_home_all['user_type'] = df_uesr_home_all['用户号码'].map(user_type_dict)
 df_uesr_home_all['user_level'] = df_uesr_home_all['用户号码'].map(user_level_dict)
 df_uesr_home_all['通话次数'] = df_uesr_home_all['用户号码'].map(user_call_count_dict)
-df_uesr_home_all['通话时长'] = df_uesr_home_all['用户号码'].map(user_call_druation_dict)
+df_uesr_home_all['通话时长'] = df_uesr_home_all['用户号码'].map(
+    user_call_druation_dict)
 df_uesr_home_all['上网流量_字节'] = df_uesr_home_all['用户号码'].map(user_data_dict)
 df_uesr_home_all['日均流量_MB'] = df_uesr_home_all['用户号码'].map(user_avg_data_dict)
 df_uesr_home_all['上网时长_秒_月'] = df_uesr_home_all['用户号码'].map(user_duration_dict)
 df_uesr_home_all['上网次数_月'] = df_uesr_home_all['用户号码'].map(user_times_dict)
 df_uesr_home_all['平均网速_KBps'] = df_uesr_home_all['用户号码'].map(user_speed_dict)
 
-df_uesr_home_high_data = df_uesr_home_all[((df_uesr_home_all['user_type'] == '数据卡用户')|(df_uesr_home_all['user_type'] == '语音数据用户'))& \
+df_uesr_home_high_data = df_uesr_home_all[((df_uesr_home_all['user_type'] == '数据卡用户') | (df_uesr_home_all['user_type'] == '语音数据用户')) &
                                           (df_uesr_home_all['user_level'] == '重度')]
 
 country_set = set(df_uesr_home_high_data['区县'])
 for country in country_set:
-     df_country = df_uesr_home_high_data[df_uesr_home_high_data['区县'] == country]
-     with pd.ExcelWriter(out_path + '各县常驻用户清单'+ '\\' +country + '_常驻纯3G用户清单.xlsx') as writer:
-          town_set = set(df_country['乡镇'])
-          for town in town_set:
-               df_town = df_country[df_country['乡镇'] == town]
-               df_town.to_excel(writer,town,index = False)
+    df_country = df_uesr_home_high_data[df_uesr_home_high_data['区县'] == country]
+    with pd.ExcelWriter(out_path + '各县常驻用户清单' + '\\' + country + '_常驻纯3G用户清单.xlsx') as writer:
+        town_set = set(df_country['乡镇'])
+        for town in town_set:
+            df_town = df_country[df_country['乡镇'] == town]
+            df_town.to_excel(writer, town, index=False)
 
 df_country_provit = pd.pivot_table(df_uesr_home_high_data,
-                                  index=['区县','user_type'],
-                                  values =['imsi'],
-                                  aggfunc = {'imsi':len})
-df_country_provit.reset_index(inplace = True)
+                                   index=['区县', 'user_type'],
+                                   values=['imsi'],
+                                   aggfunc={'imsi': len})
+df_country_provit.reset_index(inplace=True)
 
-df_town_provit_= pd.pivot_table(df_uesr_home_high_data,
-                                  index=['区县','乡镇','user_type'],
-                                  values =['imsi'],
-                                  aggfunc = {'imsi':len})
-df_town_provit_.reset_index(inplace = True)
+df_town_provit_ = pd.pivot_table(df_uesr_home_high_data,
+                                 index=['区县', '乡镇', 'user_type'],
+                                 values=['imsi'],
+                                 aggfunc={'imsi': len})
+df_town_provit_.reset_index(inplace=True)
 
 with pd.ExcelWriter(out_path + '原始数据.xlsx') as writer:
-    df_uesr_home_all.to_excel(writer,'3G用户原始数据',index =False)
+    df_uesr_home_all.to_excel(writer, '3G用户原始数据', index=False)
 
 with pd.ExcelWriter(out_path + '高流量3G用户清单.xlsx') as writer:
-    df_uesr_home_high_data.to_excel(writer,'高流量3G用户',index =False)
-    df_country_provit.to_excel(writer,'按县统计',index =False)
-    df_town_provit_.to_excel(writer,'按乡镇统计',index =False)
+    df_uesr_home_high_data.to_excel(writer, '高流量3G用户', index=False)
+    df_country_provit.to_excel(writer, '按县统计', index=False)
+    df_town_provit_.to_excel(writer, '按乡镇统计', index=False)
 
-#with pd.ExcelWriter(out_path + '用户1X通话记录.xlsx') as writer:
+# with pd.ExcelWriter(out_path + '用户1X通话记录.xlsx') as writer:
 #    df_1X_has_number.to_excel(writer,'有号码用户记录')
 #    df_1X_no_number.to_excel(writer,'无号码用户记录')
 #
-#with pd.ExcelWriter(out_path + '用户状态和IMSI.xlsx') as writer:
+# with pd.ExcelWriter(out_path + '用户状态和IMSI.xlsx') as writer:
 #    df_user_state.to_excel(writer,'用户状态')
 #    df_user_imsi.to_excel(writer,'IMSI')
-
-
-
-
