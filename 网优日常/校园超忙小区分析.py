d465294008036371fe52a897ce4eeb898060cfa4
fengmingshan <fms_python@163.com>
2019-09-06 17:19:07 +0800

    Update 校园超忙小区分析.py

--- 网优日常/校园超忙小区分析.py
+++ 网优日常/校园超忙小区分析.py
@@ -2,7 +2,7 @@
 # @Author: fengmingshan
 # @Date:   2019-09-02 15:15:02
 # @Last Modified by:   Administrator
-# @Last Modified time: 2019-09-02 16:34:02
+# @Last Modified time: 2019-09-06 17:12:31
 
 import pandas as pd
 import numpy as np
@@ -34,15 +34,57 @@ df_content['平均RRC连接用户数_1'] = df_content['平均RRC连接用户数_
 df_content['下行平均激活用户数_1'] = df_content['下行平均激活用户数_1'].map(
     lambda x: math.ceil(x))
 df_content['空口下行用户面流量（MByte）_1477070755617-11'] = df_content['空口下行用户面流量（MByte）_1477070755617-11'].map(
-    lambda x: float(x.replace(',', '')))
-df_content['分QCI用户体验下行平均速率（Mbps）_1'] = round(df_content['分QCI用户体验下行平均速率（Mbps）_1']/8,1)
+    lambda x: float(x.replace(',', '')) / 1024)
+df_content['空口上行用户面流量（MByte）_1'] = df_content['空口上行用户面流量（MByte）_1'].map(
+    lambda x: float(x.replace(',', '')) / 1024)
 
-df_content.rename(columns={'空口上行用户面流量（MByte）_1': '上行流量（MByte）',
-                           '空口下行用户面流量（MByte）_1477070755617-11': '下行流量（MByte）',
+df_content['分QCI用户体验下行平均速率（Mbps）_1'] = round(
+    df_content['分QCI用户体验下行平均速率（Mbps）_1'] / 8, 1)
+df_content['总流量(GB)'] = df_content['空口上行用户面流量（MByte）_1'] + \
+    df_content['空口下行用户面流量（MByte）_1477070755617-11']
+
+df_content.rename(columns={'空口上行用户面流量（MByte）_1': '上行流量（GB）',
+                           '空口下行用户面流量（MByte）_1477070755617-11': '下行流量（GB）',
                            '分QCI用户体验下行平均速率（Mbps）_1': '用户体验速率（MBps）',
                            }, inplace=True)
-df_content['单用户平均流量（MByte）'] = df_content['下行流量（MByte）'] / \
+df_content['单用户平均流量（MByte）'] = df_content['下行流量（GB）'] / \
     df_content['下行平均激活用户数_1']
+
+df_content['日期'] = df_content['开始时间'].apply(lambda x: x.split(' ')[0])
+df_content['小时'] = df_content['开始时间'].apply(
+    lambda x: x.split(' ')[1].split(':')[0])
+
+df_content['下行PRB平均占用率_1'] =
+
+# =============================================================================
+# 总体情况按小时分析
+# =============================================================================
+df_all = pd.pivot_table(
+    df_content,
+    index=[
+        '网元',
+        '网元名称',
+        '小区',
+        '小区名称',
+        '日期',
+        '小时'],
+    values=[
+        '最大RRC连接用户数_1',
+        '平均RRC连接用户数_1',
+        '下行平均激活用户数_1',
+        '最大激活用户数_1',
+        '总流量(GB)',
+        '下行PRB平均占用率_1'],
+
+    aggfunc={
+        '最大RRC连接用户数_1': np.sum,
+        '平均RRC连接用户数_1': np.sum,
+        '下行平均激活用户数_1': np.sum,
+        '最大激活用户数_1': np.sum,
+        '总流量(GB)': np.sum,
+        '下行PRB平均占用率_1': np.mean})
+
+
 
 # =============================================================================
 # 用户数TOP
@@ -86,10 +128,10 @@ df_max_throughput = pd.pivot_table(
            '小区名称'],
     values=['下行流量（MByte）'],
     aggfunc={
-        '下行流量（MByte）': np.max})
+        '下行流量（GB）': np.max})
 df_max_throughput.reset_index(inplace=True)
 df_max_throughput.sort_values(
-    by=['下行流量（MByte）'],
+    by=['下行流量（GB）'],
     ascending=False,
     inplace=True)
 
@@ -110,4 +152,3 @@ df_max_speed.sort_values(
     by=['用户体验速率（MBps）'],
     ascending=True,
     inplace=True)
-
