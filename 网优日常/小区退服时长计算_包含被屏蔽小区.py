a9a5364c492ae9e38d46a3b15f63f66dacc82f6c
fengmingshan <fms_python@163.com>
2019-10-17 17:44:41 +0800

    Update 小区退服时长计算_包含被屏蔽小区.py

--- 网优日常/小区退服时长计算_包含被屏蔽小区.py
+++ 网优日常/小区退服时长计算_包含被屏蔽小区.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 from dateutil.parser import parse
 from dateutil.rrule import *
-from interval import Interval,IntervalSet
+from interval import Interval, IntervalSet
 
 data_path = r'D:\_小程序\小区退服计算'
 if not os.path.exists(data_path):
@@ -19,11 +19,17 @@ block_cell = '屏蔽小区.xlsx'
 
 # 需要使用到的自定义函数
 
+
 def 填写退服小区(a, b):
     if pd.isnull(a):
         return b.split('_')[0] + '_' + b.split('_')[1]
     else:
         return a
+
+def calc_break_time(interval):
+    low = interval.lower_bound
+    upper = interval.upper_bound
+
 
 df_block = pd.read_excel(block_cell)
 df_block['退服小区标识'] = df_block.apply(
@@ -33,42 +39,47 @@ df_block.head(5)
 
 df_block = df_block[['退服小区标识', '告警发生时间', '告警清除时间', '退服时长(分钟)', '6-8点退服时长（分钟）', '8-22点退服时长（分钟）',
                      '22-24点退服时长（分钟）']]
-df_block.columns
-df_block.dtypes
 
-date_list = list(rrule(DAILY, dtstart=parse(
-    df_block.loc[0, '告警发生时间']), until=parse(df_block.loc[0, '告警清除时间'])))
-day_list = [x.strftime('%Y-%m-%d') for x in date_list]
+for i in range(len(df_block)):
+    t0 = df_block.loc[i, '告警发生时间']
+    t1 = df_block.loc[i, '告警清除时间']
+    date_list = list(rrule(DAILY, dtstart=parse(t0), until=parse(t1)))
+    day_list = [x.strftime('%Y-%m-%d') for x in date_list]
 
-start_time = datetime.strptime(df_block.loc[0, '告警发生时间'], '%Y-%m-%d %H:%M:%S')
-end_time = datetime.strptime(df_block.loc[0, '告警清除时间'], '%Y-%m-%d %H:%M:%S')
+    start_time = datetime.strptime(df_block.loc[0, '告警发生时间'], '%Y-%m-%d %H:%M:%S')
+    end_time = datetime.strptime(df_block.loc[0, '告警清除时间'], '%Y-%m-%d %H:%M:%S')
 
-break_interval = IntervalSet.between(start_time, end_time, closed=True)
-for day in day_list:
-    interval1 = IntervalSet.between(
-        datetime.strptime(
-            day + ' ' + '06:00:00',
-            '%Y-%m-%d %H:%M:%S'),
-        datetime.strptime(
-            day + ' ' + '08:00:00',
-            '%Y-%m-%d %H:%M:%S'), closed=True)
-    interval2 = IntervalSet.between(
-        datetime.strptime(
-            day + ' ' + '08:00:00',
-            '%Y-%m-%d %H:%M:%S'),
-        datetime.strptime(
-            day + ' ' + '22:00:00',
-            '%Y-%m-%d %H:%M:%S'), closed=True)
-    interval3 = IntervalSet.between(
-        datetime.strptime(
-            day + ' ' + '22:00:00',
-            '%Y-%m-%d %H:%M:%S'),
-        datetime.strptime(
-            day + ' ' + '23:59:59',
-            '%Y-%m-%d %H:%M:%S'), closed=True)
-intervla_6to8 = break_interval & interval1
-intervla_8to22 = break_interval & interval2
-intervla_22to24 = break_interval & interval3
+    # 创建
+    break_interval = IntervalSet.between(start_time, end_time, closed=True)
+    for day in day_list:
+        # 创建三个时间段，分别是6-8点，8-22点，22-24点，
+        interval_6to8 = IntervalSet.between(
+            datetime.strptime(
+                day + ' ' + '06:00:00',
+                '%Y-%m-%d %H:%M:%S'),
+            datetime.strptime(
+                day + ' ' + '08:00:00',
+                '%Y-%m-%d %H:%M:%S'), closed=True)
+        interval_8to22 = IntervalSet.between(
+            datetime.strptime(
+                day + ' ' + '08:00:00',
+                '%Y-%m-%d %H:%M:%S'),
+            datetime.strptime(
+                day + ' ' + '22:00:00',
+                '%Y-%m-%d %H:%M:%S'), closed=True)
+        interval_22to24 = IntervalSet.between(
+            datetime.strptime(
+                day + ' ' + '22:00:00',
+                '%Y-%m-%d %H:%M:%S'),
+            datetime.strptime(
+                day + ' ' + '23:59:59',
+                '%Y-%m-%d %H:%M:%S'), closed=True)
 
-interval1.lower_bound
-interval2.upper_bound
+        break_6to8 = break_interval & intervla_6to8
+        break_8to22 = break_interval & intervla_8to22
+        break_22to24 = break_interval & intervla_22to24
+        low = break_6to8.lower_bound()
+        upper = break_6to8.upper_bound()
+        break_time = upper - low
+
+
