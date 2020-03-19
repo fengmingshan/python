44ce9f6f12c247f053b2241fb5a33d674b5cbb34
fengmingshan <fms_python@163.com>
2020-03-19 22:26:54 +0800

    添加文件file_upload_excel_files

--- flask_web/File_Upload_excel_files/File_Upload_excel_files.py
+++ flask_web/File_Upload_excel_files/File_Upload_excel_files.py
@@ -1,11 +1,63 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+import os
+import time
+import hashlib
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I have a dream'
+work_path = 'D:/_python/python/flask_web/File_Upload_excel_files'
+os.chdir(work_path)
+if not os.path.exists('upload'):
+    os.mkdir('upload')
+app.config['UPLOADED_EXCELS_DEST'] = os.getcwd().replace('\\','/') + '/upload'
+
+excel_files = set(['xls', 'xlsx', 'csv'])
+excels = UploadSet('excels', excel_files)
+configure_uploads(app, excels)
+patch_request_class(app)  # set maximum file size, default is 16MB
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class UploadForm(FlaskForm):
+    excel = FileField(validators=[FileAllowed(excels, u'Only allowed Excel files!'), FileRequired(u'Choose a file!')])
+    submit = SubmitField(u'Upload')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    if form.validate_on_submit():
+        for filename in request.files.getlist('excel'):
+            excels.save(filename)
+        success = True
+    else:
+        success = False
+    return render_template('index.html', form=form, success=success)
+
+
+@app.route('/manage')
+def manage_file():
+    files_list = os.listdir(app.config['UPLOADED_EXCELS_DEST'])
+    return render_template('manage.html', files_list=files_list)
+
+
+@app.route('/open/<filename>')
+def open_file(filename):
+    file_url = excels.url(filename)
+    return render_template('browser.html', file_url=file_url)
+
+
+@app.route('/delete/<filename>')
+def delete_file(filename):
+    file_path = excels.path(filename)
+    os.remove(file_path)
+    return redirect(url_for('manage_file'))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
