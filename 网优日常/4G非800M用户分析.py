e0499de8ae41b91abdf93c77a2ffd06251bdf5d3
fengmingshan <fms_python@163.com>
2020-03-21 16:45:29 +0800

    Update 4G非800M用户分析.py

--- 网优日常/4G非800M用户分析.py
+++ 网优日常/4G非800M用户分析.py
@@ -11,9 +11,8 @@ import os
 
 work_path = 'D:/2020年工作/2020年3月非800M用户分析'
 os.chdir(work_path)
-df_not_800 = pd.read_excel('./曲靖非800m用户.xlsx.xlsx')
-num = list(set(df_not_800['后面']))
-
+df_not_800 = pd.read_excel('./曲靖非800m用户.xlsx')
+num1 = set(df_not_800['后面'])
 
 xlsx_file = pd.ExcelFile('./曲靖总流量TOP5小区用户.xlsx')
 df_list = []
@@ -21,3 +20,6 @@ for name in xlsx_file.sheet_names:
     df_tmp = pd.read_excel('./曲靖总流量TOP5小区用户.xlsx',sheet_name = name)
     df_list.append(df_tmp)
 df_resident_cell = pd.concat(df_list, axis = 0)
+df_resident_cell.columns
+num2 = set(df_resident_cell['号码'])
+num1 == num2
