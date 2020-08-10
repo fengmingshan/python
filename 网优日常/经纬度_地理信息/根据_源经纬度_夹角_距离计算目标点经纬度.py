2c1772659b4a6d2fcdad9f38db6cf133258cec34
fengmingshan <fms_python@163.com>
2020-08-10 14:51:12 +0800

    修正错误

--- 网优日常/经纬度_地理信息/根据_源经纬度_夹角_距离计算目标点经纬度.py
+++ 网优日常/经纬度_地理信息/根据_源经纬度_夹角_距离计算目标点经纬度.py
@@ -1,6 +1,6 @@
 import geopy.distance
 
-def get_distance_point(lat, lon, distance, direction):
+def calc_direction_distance_point(lat, lon, distance, direction):
     """
     根据经纬度，距离，方向获得一个地点
     :param lat: 纬度
@@ -10,8 +10,9 @@ def get_distance_point(lat, lon, distance, direction):
     :return:
     """
     start = geopy.Point(lat, lon)
-    d = geopy.distance.VincentyDistance(meters=distance)
-    return d.destination(point=start, bearing=direction)
+    d = geopy.distance.GeodesicDistance(meters=distance)
+    point = d.destination(point=start, bearing=direction)
+    return (round(point.longitude,6),round(point.latitude,6) )
 
-p = get_distance_point( 25.509586, 103.755795, 250, 60)
-print (round(p.longitude,6),round(p.latitude,6) )
+p = calc_direction_distance_point( 25.509586, 103.755795, 250, 60)
+p
