056425386adda361715f3836cfa6074baf08207f
fengmingshan <fms_python@163.com>
2020-11-01 09:14:40 +0800

    删除flask_web

--- flask_web/上传下载电子表格/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'qjwx-security-code'
-    MAX_CONTENT_LENGTH = 50 * 1024 * 1024
-    UPLOADS_DEFAULT_DEST = r'D:\_python\python\flask_web\上传下载电子表格\upload'
